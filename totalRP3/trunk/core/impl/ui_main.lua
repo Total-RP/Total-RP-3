515a1f705bd8bd98182d0aa2c301a31e178cd495
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2014-05-11 18:08:38 +0000

    Bouton de la minimap ultra light, 0 librairies, avec la dernière icône ! Et j'ai changé la highlight des boutons de la toolbar :P

--- totalRP3/trunk/core/impl/ui_main.lua
+++ totalRP3/trunk/core/impl/ui_main.lua
@@ -5,62 +5,36 @@
 --*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 -- Minimap button widget
 --*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
--- Total RP 3, by Telkostrasz & Ellypse (Kirin Tor - Eu/Fr)
---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
--- Minimap button widget
---*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+-- Config
+local getConfigValue, registerConfigKey, setConfigValue = TRP3_CONFIG.getValue, TRP3_CONFIG.registerConfigKey, TRP3_CONFIG.setValue;
+local CONFIG_MINIMAP_POS = "minimap_pos";
 
-local loc = TRP3_L;
-local switchMainFrame = TRP3_NAVIGATION.switchMainFrame;
 
--- Loading LDBIcon library
-local libDBIcon = LibStub("LibDBIcon-1.0")
+-- Reposition the minimap button using the config values
+function TRP3_MinimapButton_Reposition()
+	TRP3_MinimapButton:SetPoint("TOPLEFT","Minimap","TOPLEFT",52-(80*cos(getConfigValue(CONFIG_MINIMAP_POS))),(80*sin(getConfigValue(CONFIG_MINIMAP_POS)))-52)
+end
 
+-- Function called when the minimap icon is dragged
+function TRP3_MinimapButton_DraggingFrame_OnUpdate()
+
+	local xpos,ypos = GetCursorPosition();
+	local xmin,ymin = Minimap:GetLeft(), Minimap:GetBottom();
+
+	xpos = xmin-xpos/UIParent:GetScale()+70;
+	ypos = ypos/UIParent:GetScale()-ymin-70;
+
+	-- Setting the minimap coordanate
+	setConfigValue(CONFIG_MINIMAP_POS,math.deg(math.atan2(ypos,xpos))); 
+
+	TRP3_MinimapButton_Reposition()
+end
 
 -- Initialize the minimap icon button
 function TRP3_InitMinimapButton(addon)
-
-	-- Create the minimap button, a LibDB object
-	local TRP3_MinimapButton = LibStub("LibDataBroker-1.1"):NewDataObject("TRP3_MinimapButton", {
-		type = "launcher", 	-- Indicates that this object is a launcher data source and does not provide any data to be rendered in an always-up frame
-		tocname = "totalRP3", -- The name of the addon providing the launcher. Used by displays to get TOC metadata about the addon
-		icon = "Interface\\AddOns\\totalRP3\\resources\\trp3minimap.tga",
-		OnClick = function(clickedframe, button)			
-			if button == "RightButton" then
-				TRP3_SwitchToolbar();
-			else
-				switchMainFrame();
-			end
-		end,
-		OnLoad = function() self:RegisterForClicks("LeftButtonUp","RightButtonUp"); end,
-		OnTooltipShow = function(tooltip)
-		-- Function called by the object to display its tooltip.
-		-- The display will manage positioning, clearing and showing the tooltip, 
-		-- all this handler needs to do is populate the tooltip using :AddLine or similar. 
-	    TRP3_MinimapTooltip(tooltip)
-	    end,
-	});
-
-	-- Create a saved variable for the minimap object
-	-- Using profile means every character can have the minimap button
-	-- in a different place
-	addon.db = LibStub("AceDB-3.0"):New("TRP3_Configuration", {
-		profile = {
-			minimap = {
-				hide = false,
-			},
-		},
-	});
-	libDBIcon:Register("TRP3_MinimapButton", TRP3_MinimapButton, addon.db.profile.minimap)
+	registerConfigKey(CONFIG_MINIMAP_POS, 202);
+	TRP3_MinimapButton_Reposition();
 end
 
--- Populates the LDB tooltip
-function TRP3_MinimapTooltip(tooltip)
-	tooltip:AddLine("Total RP 3")
-    tooltip:AddLine(loc("CM_R_CLICK").." : "..loc("MM_SHOW_HIDE_MAIN"));
-    tooltip:AddLine(loc("CM_L_CLICK").." : "..loc("MM_SHOW_HIDE_SHORTCUT"));
-end
 
