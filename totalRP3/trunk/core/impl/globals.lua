bd1a17ef903e9482130a0b5211a8a2906c2e6667
Sylvain "Telkostrasz" Cossement <telkostrasz@totalrp3.info>
2014-11-22 15:43:41 +0000

    0.2.6-SNAPSHOT

--- totalRP3/trunk/core/impl/globals.lua
+++ totalRP3/trunk/core/impl/globals.lua
@@ -33,7 +33,7 @@ TRP3_API = {
 		addon_id_length = 15,
 
 		version = 8,
-		version_display = "0.2.5",
+		version_display = "0.2.6-SNAPSHOT",
 
 		player = UnitName("player"),
 		player_realm = GetRealmName(),
