ed4368aacbb19e5c524529a407a0bd20606a4b35
Sylvain "Telkostrasz" Cossement <telkostrasz@totalrp3.info>
2014-11-23 19:39:04 +0000

    Initial commit

---
+++ totalRP3_Storyline/trunk/totalRP3_Storyline.lua
@@ -0,0 +1,289 @@
+----------------------------------------------------------------------------------
+-- Total RP 3
+-- Storyline module
+--	---------------------------------------------------------------------------
+--	Copyright 2014 Sylvain Cossement (telkostrasz@telkostrasz.be)
+--
+--	Licensed under the Apache License, Version 2.0 (the "License");
+--	you may not use this file except in compliance with the License.
+--	You may obtain a copy of the License at
+--
+--		http://www.apache.org/licenses/LICENSE-2.0
+--
+--	Unless required by applicable law or agreed to in writing, software
+--	distributed under the License is distributed on an "AS IS" BASIS,
+--	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--	See the License for the specific language governing permissions and
+--	limitations under the License.
+----------------------------------------------------------------------------------
+
+assert(TRP3_API ~= nil, "Can't find Total RP 3 API.");
+
+-- imports
+local Globals, Utils, Comm, Events = TRP3_API.globals, TRP3_API.utils, TRP3_API.communication, TRP3_API.events;
+local TRP3_NPCDialogFrame = TRP3_NPCDialogFrame;
+local TRP3_NPCDialogFrameModelsMe, TRP3_NPCDialogFrameModelsYou = TRP3_NPCDialogFrameModelsMe, TRP3_NPCDialogFrameModelsYou;
+local TRP3_NPCDialogFrameChat, TRP3_NPCDialogFrameChatText = TRP3_NPCDialogFrameChat, TRP3_NPCDialogFrameChatText;
+local tostring, strsplit, wipe = tostring, strsplit, wipe;
+local ChatTypeInfo, GetGossipText, GetGreetingText, GetProgressText = ChatTypeInfo, GetGossipText, GetGreetingText, GetProgressText;
+local GetRewardText, GetQuestText = GetRewardText, GetQuestText;
+
+--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+-- LOGIC
+--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+local ANIMATION_SEQUENCE_SPEED = 1000;
+local ANIMATION_TEXT_SPEED = 40;
+local ANIMATION_SEQUENCE_DURATION = {
+	["64"] = 3000,
+	["65"] = 3000,
+	["60"] = 4000,
+	["0"] = 2000,
+}
+local ANIMATION_EMPTY = {0};
+local animTab = {};
+local LINE_FEED_CODE = string.char(10);
+local CARRIAGE_RETURN_CODE = string.char(13);
+local WEIRD_LINE_BREAK = LINE_FEED_CODE .. CARRIAGE_RETURN_CODE .. LINE_FEED_CODE;
+
+local EVENT_INFO = {
+	["QUEST_GREETING"] = {
+		text = GetGreetingText,
+		cancelMethod = CloseQuest,
+	},
+	["QUEST_DETAIL"] = {
+		text = GetQuestText,
+		finishMethod = AcceptQuest,
+		cancelMethod = DeclineQuest,
+		finishText = ACCEPT,
+	},
+	["QUEST_PROGRESS"] = {
+		text = GetProgressText,
+		finishMethod = function()
+			if IsQuestCompletable() then
+				CompleteQuest();
+			else
+				CloseQuest();
+			end
+		end,
+		finishText = function()
+			if IsQuestCompletable() then
+				return CONTINUE;
+			else
+				return GOODBYE;
+			end
+		end,
+		cancelMethod = CloseQuest,
+	},
+	["QUEST_COMPLETE"] = {
+		text = GetRewardText,
+		finishText = function()
+			if GetNumQuestChoices() > 0 then
+				return REWARDS;
+			else
+				return COMPLETE_QUEST;
+			end
+		end,
+		finishMethod = function()
+			if GetNumQuestChoices() > 0 then
+				message("Please choose a reward from the original quest interface.");
+			else
+				GetQuestReward();
+			end
+		end,
+		cancelMethod = CloseQuest,
+	},
+	["GOSSIP_SHOW"] = {
+		text = GetGossipText,
+		finishMethod = CloseGossip,
+		finishText = GOODBYE,
+		cancelMethod = CloseGossip,
+	},
+}
+
+local CHAT_MARGIN = 70;
+
+local function playText(textIndex)
+	local text = TRP3_NPCDialogFrameChat.texts[textIndex];
+	local sound;
+	wipe(animTab);
+	text:gsub("[%.%?%!]+", function(finder)
+		if finder:sub(1, 1) == "!" then
+			animTab[#animTab+1] = 64;
+		elseif finder:sub(1, 1) == "?" then
+			animTab[#animTab+1] = 65;
+		else
+			animTab[#animTab+1] = 60;
+		end
+	end);
+	animTab[#animTab+1] = 0;
+	
+	if text:byte() == 60 then -- Emote if begins with <
+		TRP3_NPCDialogFrameChatText:SetTextColor(ChatTypeInfo["MONSTER_EMOTE"].r, ChatTypeInfo["MONSTER_EMOTE"].g, ChatTypeInfo["MONSTER_EMOTE"].b);
+		TRP3_NPCDialogFrameChatText:SetText(text:sub(2, text:len() - 1));
+		wipe(animTab);
+		animTab[1] = 0;
+	else
+		TRP3_NPCDialogFrameChatText:SetTextColor(ChatTypeInfo["MONSTER_SAY"].r, ChatTypeInfo["MONSTER_SAY"].g, ChatTypeInfo["MONSTER_SAY"].b);
+		TRP3_NPCDialogFrameChatText:SetText(text);
+	end
+
+	TRP3_NPCDialogFrameChat:SetHeight(TRP3_NPCDialogFrameChatText:GetHeight() + CHAT_MARGIN);
+
+	TRP3_NPCDialogFrameModelsYou.seqtime = 0;
+	TRP3_NPCDialogFrameModelsYou.sequenceTab = animTab;
+	TRP3_NPCDialogFrameModelsYou.sequence = 1;
+	
+	TRP3_NPCDialogFrameChat.start = 0;
+
+	if textIndex > 1 then
+		TRP3_NPCDialogFrameChatPrevious:Show();
+	end
+
+end
+
+local function playNext()
+	TRP3_NPCDialogFrameChat.currentIndex = TRP3_NPCDialogFrameChat.currentIndex + 1;
+	if TRP3_NPCDialogFrameChat.currentIndex <= #TRP3_NPCDialogFrameChat.texts then
+		playText(TRP3_NPCDialogFrameChat.currentIndex);
+		if TRP3_NPCDialogFrameChat.currentIndex < #TRP3_NPCDialogFrameChat.texts then
+			TRP3_NPCDialogFrameChatNext:SetText("Next");
+		else
+			if type(TRP3_NPCDialogFrameChat.eventInfo.finishText) == "function" then
+				TRP3_NPCDialogFrameChatNext:SetText(TRP3_NPCDialogFrameChat.eventInfo.finishText());
+			else
+				TRP3_NPCDialogFrameChatNext:SetText(TRP3_NPCDialogFrameChat.eventInfo.finishText or "Finish");
+			end
+		end
+	else
+		if TRP3_NPCDialogFrameChat.eventInfo.finishMethod then
+			TRP3_NPCDialogFrameChat.eventInfo.finishMethod();
+		else
+			TRP3_NPCDialogFrame:Hide();
+		end
+	end
+end
+
+local function closeDialog()
+	if TRP3_NPCDialogFrameChat.eventInfo and TRP3_NPCDialogFrameChat.eventInfo.cancelMethod then
+		TRP3_NPCDialogFrameChat.eventInfo.cancelMethod();
+	else
+		TRP3_NPCDialogFrame:Hide();
+	end
+end
+
+local function resetDialog()
+	TRP3_NPCDialogFrameChat.currentIndex = 0;
+	playNext();
+end
+
+local function startDialog(targetType, fullText, event, eventInfo)
+	-- Ca marche pas, merci Blizzard d'ajouter des fonctions que tu n'impl�mentes pas toi m�me.
+--	local forced = ForceGossip();
+--	if not forced then
+--		return;
+--	end
+
+	TRP3_NPCDialogFrameDebug:SetText("[debug] " .. event);
+
+	TRP3_NPCDialogFrameModelsYou:SetCamera(1);
+	TRP3_NPCDialogFrameModelsYou:SetFacing(-0.75);
+	TRP3_NPCDialogFrameModelsYou:SetUnit(targetType);
+	TRP3_NPCDialogFrameModelsYou:SetLight(1, 0, 0, 1, 1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+	
+	fullText = fullText:gsub(LINE_FEED_CODE .. "+", "\n");
+	fullText = fullText:gsub(WEIRD_LINE_BREAK, "\n");
+	
+	local texts = {strsplit("\n", fullText)};
+	TRP3_NPCDialogFrameChat.texts = texts;
+	TRP3_NPCDialogFrameChat.currentIndex = 0;
+	TRP3_NPCDialogFrameChat.eventInfo = eventInfo;
+
+	TRP3_NPCDialogFrameChatPrevious:Hide();
+	
+	TRP3_NPCDialogFrame:Show();
+
+	playNext();
+end
+
+--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+-- ANIMATIONS
+--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+local function onUpdateModel(self, elapsed)
+	if self.seqtime and self.sequence and self.sequenceTab then
+		self.seqtime = self.seqtime + (elapsed * ANIMATION_SEQUENCE_SPEED);
+		if self.sequenceTab[self.sequence] ~= 0 then
+			self:SetSequenceTime(self.sequenceTab[self.sequence], self.seqtime);
+		end
+		local sequenceString = tostring(self.sequenceTab[self.sequence]);
+		-- Once the anim is finished, go to the next one.
+		if ANIMATION_SEQUENCE_DURATION[sequenceString] and self.seqtime > ANIMATION_SEQUENCE_DURATION[sequenceString] then
+			self.seqtime = 0;
+			if self.sequence < #self.sequenceTab then
+				self.sequence = self.sequence + 1;
+			end
+		end
+	end
+end
+
+local function onUpdateChatText(self, elapsed)
+	if self.start then
+		self.time = 0;
+		self.start = self.start + (elapsed * ANIMATION_TEXT_SPEED);
+		if self.start == TRP3_NPCDialogFrameChatText:GetText():len() then
+			self.start = nil;
+			TRP3_NPCDialogFrameChatText:SetAlphaGradient(0,0);
+		else
+			TRP3_NPCDialogFrameChatText:SetAlphaGradient(self.start, 30);
+		end
+	end
+end
+
+--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+-- INIT
+--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+local function init()
+	ForceGossip = function() return true end
+
+	TRP3_NPCDialogFrameChatText:SetWidth(500);
+	TRP3_NPCDialogFrameModelsMe:SetCamera(1);
+	TRP3_NPCDialogFrameModelsMe:SetFacing(0.75);
+	TRP3_NPCDialogFrameModelsMe:SetUnit("player");
+	TRP3_NPCDialogFrameModelsMe:SetLight(1, 0, 0, -1, -1, 1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0);
+
+	TRP3_NPCDialogFrameChatPrevious:SetText("Reset");
+
+	TRP3_NPCDialogFrameChatNext:SetScript("OnClick", playNext);
+	TRP3_NPCDialogFrameChatPrevious:SetScript("OnClick", resetDialog);
+	TRP3_NPCDialogFrameModelsYou:SetScript("OnUpdate", onUpdateModel);
+	TRP3_NPCDialogFrameChat:SetScript("OnUpdate", onUpdateChatText);
+	TRP3_NPCDialogClose:SetScript("OnClick", closeDialog);
+
+	-- Showing
+	for event, info in pairs(EVENT_INFO) do
+		Utils.event.registerHandler(event, function()
+			startDialog("npc", info.text(), event, info);
+		end);
+	end
+
+	-- Closing
+	Utils.event.registerHandler("GOSSIP_CLOSED", function()
+		TRP3_NPCDialogFrame:Hide();
+	end);
+	Utils.event.registerHandler("QUEST_FINISHED", function()
+		TRP3_NPCDialogFrame:Hide();
+	end);
+end
+
+local MODULE_STRUCTURE = {
+	["name"] = "Storyline",
+	["description"] = "Enhance the way questlines are told.",
+	["version"] = 1.000,
+	["id"] = "better_npc_chat",
+	["onStart"] = init,
+	["minVersion"] = 0.1,
+};
+
+TRP3_API.module.registerModule(MODULE_STRUCTURE);
