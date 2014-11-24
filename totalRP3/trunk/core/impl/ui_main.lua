f1f02b62a26dfa6167d4b852fb7713b9eb7ca5fe
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2014-11-24 18:45:14 +0000

    TRP-98 Bouton de la minimap utilisant LDB Version finale (normalement 😜)

--- totalRP3/trunk/core/impl/ui_main.lua
+++ totalRP3/trunk/core/impl/ui_main.lua
@@ -22,108 +22,80 @@
 --*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 -- Config
-local Utils = TRP3_API.utils;
 local displayMessage = TRP3_API.utils.message.displayMessage;
-local getConfigValue, registerConfigKey, registerConfigHandler, setConfigValue = TRP3_API.configuration.getValue, TRP3_API.configuration.registerConfigKey, TRP3_API.configuration.registerHandler, TRP3_API.configuration.setValue;
-local math, GetCursorPosition, Minimap, UIParent, cos, sin, strconcat = math, GetCursorPosition, Minimap, UIParent, cos, sin, strconcat;
-local setTooltipAll = TRP3_API.ui.tooltip.setTooltipAll;
-local color, loc, tinsert, _G = TRP3_API.utils.str.color, TRP3_API.locale.getText, tinsert, _G;
-local CONFIG_MINIMAP_FRAME, CONFIG_MINIMAP_X, CONFIG_MINIMAP_Y, CONFIG_MINIMAP_LOCK = "minimap_frame", "minimap_x", "minimap_y", "minimap_lock";
-local minimapButton;
+local CONFIG_MINIMAP_SHOW = "minimap_show";
+local CONFIG_MINIMAP_POSITION = "minimap_icon_position";
+local getConfigValue, registerConfigKey = TRP3_API.configuration.getValue, TRP3_API.configuration.registerConfigKey;
+local color, loc, strconcat = TRP3_API.utils.str.color, TRP3_API.locale.getText, strconcat;
 
-local function getParentFrame()
-	return _G[getConfigValue(CONFIG_MINIMAP_FRAME)] or Minimap;
+-- Minimap button API initialization
+TRP3_API.navigation.minimapicon = {};
+
+local LDBObject;
+local icon;
+
+-- Initialize LDBIcon and display the minimap button
+local showMinimapButton = function()
+	icon:Show("Total RP 3");
 end
+TRP3_API.navigation.minimapicon.show = showMinimapButton;
 
--- Reposition the minimap button using the config values
-local function minimapButton_Reposition()
-	local parentFrame = getParentFrame();
-	local parentScale = UIParent:GetEffectiveScale();
-	minimapButton:SetParent(parentFrame);
-	minimapButton:ClearAllPoints();
-	minimapButton:SetPoint("CENTER", parentFrame, "BOTTOMLEFT", getConfigValue(CONFIG_MINIMAP_X) / parentScale, getConfigValue(CONFIG_MINIMAP_Y) / parentScale);
+-- Hide the minimap button and release LDBIcon from the memory
+local hideMinimapButton = function()
+	icon:Hide("Total RP 3");
 end
+TRP3_API.navigation.minimapicon.hide = hideMinimapButton;
 
--- Function called when the minimap icon is dragged
-local function minimapButton_DraggingFrame_OnUpdate(self)
-	if not getConfigValue(CONFIG_MINIMAP_LOCK) and self.isDraging then
-		local parentFrame = getParentFrame();
-		local scaleFactor = UIParent:GetEffectiveScale();
-		local xpos, ypos = GetCursorPosition();
-		local xmin, ymin = parentFrame:GetLeft(), parentFrame:GetBottom();
-
-		xpos = xpos - xmin * scaleFactor;
-		ypos = ypos - ymin * scaleFactor;
-
-		-- Setting the minimap coordinates
-		setConfigValue(CONFIG_MINIMAP_X, xpos);
-		setConfigValue(CONFIG_MINIMAP_Y, ypos);
-
-		minimapButton_Reposition();
-	end
-end
-
-local function resetPosition()
-	setConfigValue(CONFIG_MINIMAP_X, 0);
-	setConfigValue(CONFIG_MINIMAP_Y, 0);
-	minimapButton_Reposition();
-end
-
--- Initialize the minimap icon button
 TRP3_API.events.listenToEvent(TRP3_API.events.WORKFLOW_ON_LOADED, function()
-	local toggleMainPane = TRP3_API.navigation.switchMainFrame;
-	minimapButton = TRP3_MinimapButton;
 
-	registerConfigKey(CONFIG_MINIMAP_FRAME, "Minimap");
-	registerConfigKey(CONFIG_MINIMAP_LOCK, false);
-	registerConfigKey(CONFIG_MINIMAP_X, 22);
-	registerConfigKey(CONFIG_MINIMAP_Y, 5);
-	
-	registerConfigHandler(CONFIG_MINIMAP_FRAME, function()
-		minimapButton_Reposition();
-	end);
+	registerConfigKey(CONFIG_MINIMAP_SHOW, true);
+	registerConfigKey(CONFIG_MINIMAP_POSITION, {});
 
-	tinsert(TRP3_API.configuration.CONFIG_FRAME_PAGE.elements, {
-		inherit = "TRP3_ConfigH1",
-		title = loc("CO_MINIMAP_BUTTON"),
-	});
-	tinsert(TRP3_API.configuration.CONFIG_FRAME_PAGE.elements, {
-		inherit = "TRP3_ConfigEditBox",
-		title = loc("CO_MINIMAP_BUTTON_FRAME"),
-		help = loc("CO_MINIMAP_BUTTON_FRAME_TT"),
-		configKey = CONFIG_MINIMAP_FRAME,
-	});
+
 	tinsert(TRP3_API.configuration.CONFIG_FRAME_PAGE.elements, {
 		inherit = "TRP3_ConfigCheck",
-		title = loc("CO_MINIMAP_BUTTON_LOCK"),
-		help = loc("CO_MINIMAP_BUTTON_LOCK_TT"),
-		configKey = CONFIG_MINIMAP_LOCK,
-	});
-	tinsert(TRP3_API.configuration.CONFIG_FRAME_PAGE.elements, {
-		inherit = "TRP3_ConfigButton",
-		title = loc("CO_MINIMAP_BUTTON_RESET"),
-		help = loc("CO_MINIMAP_BUTTON_RESET_TT"),
-		text = loc("CO_MINIMAP_BUTTON_RESET_BUTTON"),
-		callback = resetPosition,
+		title = "Show minimap button",
+		help = "If you are using an other add-on to display Total RP 3's minimap button (FuBar, Titan, Bazooka) you can remove the button from the minimap.\n\n|cff00ff00Reminder : You can open Total RP 3 using /trp3 switch main|r",
+		configKey = CONFIG_MINIMAP_SHOW,
 	});
 
-	minimapButton:SetScript("OnUpdate", minimapButton_DraggingFrame_OnUpdate);
-	minimapButton:SetScript("OnClick", function(self, button)
-		if button == "RightButton" and TRP3_API.toolbar then
-			TRP3_API.toolbar.switch();
+	TRP3_API.configuration.registerHandler(CONFIG_MINIMAP_SHOW, function()
+		if getConfigValue(CONFIG_MINIMAP_SHOW) then
+			showMinimapButton();
 		else
-			toggleMainPane();
+			hideMinimapButton();
 		end
 	end);
-
-	minimapButton_Reposition();
 
 	local minimapTooltip = strconcat(color("y"), loc("CM_L_CLICK"), ": ", color("w"), loc("MM_SHOW_HIDE_MAIN"));
 	if TRP3_API.toolbar then
 		minimapTooltip = strconcat(minimapTooltip, "\n", color("y"), loc("CM_R_CLICK"), ": ", color("w"), loc("MM_SHOW_HIDE_SHORTCUT"));
 	end
 	minimapTooltip = strconcat(minimapTooltip, "\n", color("y"), loc("CM_DRAGDROP"), ": ", color("w"), loc("MM_SHOW_HIDE_MOVE"));
-	setTooltipAll(minimapButton, "BOTTOMLEFT", 0, 0, "Total RP 3", minimapTooltip);
+
+	LDBObject = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Total RP 3", {
+		type = "launcher",
+		icon = "Interface\\AddOns\\totalRP3\\resources\\trp3minimap.tga",
+		tocname = "totalRP3",
+		OnClick = function(clickedframe, button)
+			if button == "RightButton" and TRP3_API.toolbar then
+				TRP3_API.toolbar.switch();
+			else
+				TRP3_API.navigation.switchMainFrame();
+			end
+		end,
+		OnTooltipShow = function(tooltip)
+			tooltip:AddLine("Total RP 3");
+			tooltip:AddLine(minimapTooltip);
+		end,
+	})
+
+	icon = LibStub("LibDBIcon-1.0");
+	icon:Register("Total RP 3", LDBObject, getConfigValue(CONFIG_MINIMAP_POSITION))
+
+	if getConfigValue(CONFIG_MINIMAP_SHOW) then
+		showMinimapButton();
+	end
 
 	-- Slash command to switch frames
 	TRP3_API.slash.registerCommand({
@@ -150,11 +122,6 @@ TRP3_API.events.listenToEvent(TRP3_API.events.WORKFLOW_ON_LOADED, function()
 			if arg1 ~= "frames" then
 				displayMessage(loc("COM_RESET_USAGE"));
 			else
-				-- Minimap button
-				setConfigValue(CONFIG_MINIMAP_FRAME, "Minimap");
-				setConfigValue(CONFIG_MINIMAP_LOCK, false);
-				setConfigValue(CONFIG_MINIMAP_X, 22);
-				setConfigValue(CONFIG_MINIMAP_Y, 5);
 				-- Target frame
 				if TRP3_API.target then
 					TRP3_API.target.reset();
