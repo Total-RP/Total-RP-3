af2c86cc0d722d8414cc8f417835241f171947f8
Sylvain Telkostrasz Cossement <telkostrasz@totalrp3.info>
2016-04-28 17:54:29 +0200

    Adapt ID generation

--- totalRP3/core/impl/utils.lua
+++ totalRP3/core/impl/utils.lua
@@ -252,23 +252,11 @@ end
 local function generateID()
 	local ID = date("%m%d%H%M%S");
 	for i=1, 5 do
-		ID = ID..string.char(math.random(33,126));
+		ID = ID..string.char(math.random(75,126));
 	end
 	return ID;
 end
 Utils.str.id = generateID;
-
---	Generate a pseudo-unique random ID.
---  If you encounter a collision, you really should playing lottery
---	ID's have a id_length characters length
-local function generateObjectID(db, type)
-	local ID = date("%m%d%H%M%S");
-	for i=1, 3 do
-		ID = ID..string.char(math.random(33,126));
-	end
-	return db .. type .. ID;
-end
-Utils.str.oId = generateObjectID;
 
 -- Create a unit ID from a unit name and unit realm. If realm = nil then we use current realm.
 -- This method ALWAYS return a nil free UnitName-RealmShortName string.
