99d73a5173ebedfa67e44a073a064213db083211
Sylvain Telkostrasz Cossement <telkostrasz@totalrp3.info>
2016-05-14 15:38:13 +0200

    No more TRP3 logo in popups

--- totalRP3/core/impl/popup.lua
+++ totalRP3/core/impl/popup.lua
@@ -126,7 +126,8 @@ StaticPopupDialogs["TRP3_INPUT_NUMBER"] = {
 -- Static popups methods
 --*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-local POPUP_HEAD = "|TInterface\\AddOns\\totalRP3\\resources\\trp3logo:113:263|t\n \n";
+-- local POPUP_HEAD = "|TInterface\\AddOns\\totalRP3\\resources\\trp3logo:113:263|t\n \n";
+local POPUP_HEAD = "Total RP 3\n \n";
 
 -- Show a simple alert with a OK button.
 function TRP3_API.popup.showAlertPopup(text)
