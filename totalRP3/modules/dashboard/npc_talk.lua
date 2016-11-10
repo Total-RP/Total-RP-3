4cd57ebc881f89fc41b76bb18ede9b4a78792d62
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2016-11-10 19:49:27 +0100

    Finished NPC talk \o/

--- totalRP3/modules/dashboard/npc_talk.lua
+++ totalRP3/modules/dashboard/npc_talk.lua
@@ -23,84 +23,116 @@ TRP3_API.events.listenToEvent(TRP3_API.events.WORKFLOW_ON_LOADED, function()
 
 	local loc = TRP3_API.locale.getText;
 	local getConfigValue = TRP3_API.configuration.getValue;
+	local displayMessage = TRP3_API.utils.message.displayMessage;
+	local setTooltipForSameFrame = TRP3_API.ui.tooltip.setTooltipForSameFrame;
+	local setupMovableFrame = TRP3_API.ui.frame.setupMove;
+	local setupListBox = TRP3_API.ui.listbox.setupListBox;
+	local SendChatMessage = SendChatMessage;
+	local strtrim, pairs, tinsert = strtrim, pairs, tinsert;
+	local ChatTypeInfo = ChatTypeInfo;
+	local messageTypes = TRP3_API.utils.message.type;
+	local colorTool = CreateColor();
 
+	local frame = TRP3_NPCTalk;
 	local CONFIG_NPC_TALK_PREFIX = "chat_npc_talk_p";
 
-	local CHANNEL_TYPE = {
-		SAY = "SAY",
-		EMOTE = "EMOTE",
-		YELL = "YELL"
+	local CHANNEL_TYPES = {
+		SAY = "MONSTER_SAY",
+		EMOTE = "MONSTER_EMOTE",
+		YELL = "MONSTER_YELL",
+		WHISPER = "MONSTER_WHISPER"
 	}
 
-	TRP3_NPCTalk.title:SetText("NPC talk");
-	TRP3_NPCTalk.channelLabel:SetText("Channel");
-	TRP3_NPCTalk.nameLabel:SetText("NPC name");
-	TRP3_NPCTalk.messageLabel:SetText("Message");
-	TRP3_NPCTalk.send:SetText("Send");
+	local function sendNPCTalk()
 
-	TRP3_API.ui.tooltip.setTooltipForSameFrame(TRP3_NPCTalk.nameText.help, "RIGHT", 0, 5, "NPC Name", "You can use standard chat tags like %t to insert your target's name or %f to insert your focus' name.\n\nYou can also leave this field empty to create emotes without an NPC name at the start.");
-
-	TRP3_NPCTalk.nameText:SetText("Anna");
-	TRP3_NPCTalk.messageText.scroll.text:SetText("Do you wanna build a snowman?");
-
-	TRP3_API.ui.frame.setupMove(TRP3_NPCTalk);
-
-	TRP3_NPCTalk.send:SetScript("OnClick", function()
-		local NPCName = TRP3_NPCTalk.nameText:GetText();
-		local channel = TRP3_NPCTalk.channelDropdown:GetSelectedValue();
-		local message = TRP3_NPCTalk.messageText.scroll.text:GetText();
+		local NPCName = strtrim(frame.name:GetText());
+		local channel = frame.channelDropdown:GetSelectedValue();
+		local message = frame.messageText.scroll.text:GetText();
 
 		if not message or message == "" then
-			message("The message cannot be empty");
+			return displayMessage(loc("NPC_TALK_ERROR_EMPTY_MESSAGE"), messageTypes.ALERT_MESSAGE);
 		end
 
 		if NPCName and NPCName ~= "" then
-			if channel == CHANNEL_TYPE.SAY then
+			if channel == CHANNEL_TYPES.SAY then
 				NPCName = NPCName .. " " .. loc("NPC_TALK_SAY_PATTERN");
+			elseif channel == CHANNEL_TYPES.YELL then
+				NPCName = NPCName .. " " .. loc("NPC_TALK_YELL_PATTERN");
+			elseif channel == CHANNEL_TYPES.WHISPER then
+				NPCName = NPCName .. " " .. loc("NPC_TALK_WHISPER_PATTERN");
+			elseif channel == CHANNEL_TYPES.EMOTE then
+				NPCName = NPCName .. " ";
 			end
-
 			message = NPCName .. message;
 		end
 
 		message = getConfigValue(CONFIG_NPC_TALK_PREFIX) .. message;
 
-		-- If the channel is Say, we will send it via emote anyway so it is not shown in the chat bubble on the player's head
-		if channel == CHANNEL_TYPE.SAY then
-			channel = CHANNEL_TYPE.EMOTE
+		SendChatMessage(message, "EMOTE");
+		frame.messageText.scroll.text:SetText("");
+
+	end
+
+	local function buildChannelDropdown()
+		local channelTypes = {};
+
+		for channelLabel, channelName in pairs(CHANNEL_TYPES) do
+			local chatInfo = ChatTypeInfo[channelName];
+			colorTool:SetRGB(chatInfo.r, chatInfo.g, chatInfo.b)
+			tinsert(channelTypes, {"|cfff2bf03Channel:|r " .. colorTool:WrapTextInColorCode(_G[channelLabel]), channelName});
 		end
 
-		SendChatMessage(message, channel);
-	end)
+		setupListBox(frame.channelDropdown, channelTypes, nil, nil, 120, false);
+		frame.channelDropdown:SetSelectedValue(CHANNEL_TYPES.SAY);
+	end
 
-	TRP3_API.ui.listbox.setupListBox(TRP3_NPCTalk.channelDropdown, {
-		{SAY, CHANNEL_TYPE.SAY},
-		{EMOTE, CHANNEL_TYPE.EMOTE},
-		{YELL, CHANNEL_TYPE.YELL}
-	}, nil, nil, 145, false);
+	---
+	-- Display the frame and do everything that needs to be done at show time
+	local function showNPCTalkFrame()
+		-- We always rebuild the dropdown on show as some of the colors can change during the session
+		buildChannelDropdown();
+		frame:Show();
+	end
 
-	TRP3_NPCTalk.channelDropdown:SetSelectedValue("SAY");
+	frame.send:SetScript("OnClick", sendNPCTalk);
+	frame.messageText.scroll.text:SetScript("OnEnterPressed", sendNPCTalk);
+	frame.title:SetText(loc("NPC_TALK_TITLE"));
+	frame.name.title:SetText(loc("NPC_TALK_NAME"));
+	frame.messageLabel:SetText(loc("NPC_TALK_MESSAGE"));
+	frame.send:SetText(loc("NPC_TALK_SEND"));
+
+	setTooltipForSameFrame(frame.name.help, "RIGHT", 0, 5, loc("NPC_TALK_NAME"), loc("NPC_TALK_NAME_TT"));
+
+	setupMovableFrame(frame);
+
 
 	if TRP3_API.toolbar then
 
+		-- Create a button for the toolbar to show/hide the NPC Talk frame
 		local button = {
-			id = "aa_trp3_npctalk",
+			id = "bb_trp3_npctalk",
 			icon = "Ability_Warrior_CommandingShout",
-			configText = "",
-			onEnter = function(Uibutton, buttonStructure) end,
-			onUpdate = function(Uibutton, buttonStructure)
-
-			end,
+			configText = loc("NPC_TALK_TITLE"),
+			tooltip = loc("NPC_TALK_TITLE"),
+			tooltipSub = loc("NPC_TALK_BUTTON_TT"),
 			onClick = function(Uibutton, buttonStructure, button)
-				if TRP3_NPCTalk:IsShown() then
-					TRP3_NPCTalk:Hide();
+				if frame:IsShown() then
+					frame:Hide();
 				else
-					TRP3_NPCTalk:Show();
+					showNPCTalkFrame();
 				end
-			end,
-			onLeave = function()
 			end,
 		};
 
 		TRP3_API.toolbar.toolbarAddButton(button);
 	end
+
+	-- We also register a slash command, so people who disable the toolbar button
+	-- or the toolbar module entirely can still access the feature
+	TRP3_API.slash.registerCommand({
+		id = "npc_talk",
+		helpLine = " " .. loc("NPC_TALK_COMMAND_HELP"),
+		handler = showNPCTalkFrame
+	});
+
 end);
