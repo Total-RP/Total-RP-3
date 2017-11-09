4357131313a858c6bf4229d1ce2c16d32e88322b
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2017-11-09 18:41:57 +0100

    Updated in-game changelog

--- totalRP3/core/impl/locale/locale_enUS.lua
+++ totalRP3/core/impl/locale/locale_enUS.lua
@@ -983,19 +983,24 @@ Your profiles, companions profiles and settings will be temporarily stashed away
 |cff00ff00Use the same command again (|cff999999/trp3 stash|cff00ff00) to restore your data.|r]],
 		OPTION_ENABLED_TOAST = "Option enabled",
 		OPTION_DISABLED_TOAST = "Option disabled",
-		WHATS_NEW_15 = [[
-## 1.2.10 - 2017-09-28
+		WHATS_NEW_16 = [[
+## 1.2.11 - 2017-11-09
 
 ### Added
 
-- Added 494 icons to our icon browser, from the latest patches.
-- Added 185 musics to our music browser. We were finally able to find the musics for patch 7.1, 7.2 and 7.3, including Karazhan, Winter Veil, Diablo Anniversary, Tomb of Sargeras, Argus and more!
-- Added 142 images to our image browser, including the new prestige icons, PvP emblems, the new dressing room backgrounds, and even the calendar icon for the [Running of the Gnomes event](http://bit.ly/GnomeRun2k17)!
-- Added thank you message for Ellypse's Patreon supporters.
+- Added support for the profile downloading indicator from and to the XRP add-on.
 
 ### Fixed
 
-- Improved the workaround for the text cursor misalignment issue brought by patch 7.3 in the EditBox. The text no longer go beyond the limits of the EditBox.
+- Fixed an error when trying to whitelist a profile that has been flagged as containing mature content when the profile hasn't been entirely downloaded yet ([ticket #133](https://wow.curseforge.com/projects/total-rp-3/issues/133)).
+- Fixed an issue allowing the user to send empty NPC messages when using the Enter key ([ticket #124](https://wow.curseforge.com/projects/total-rp-3/issues/124)).
+- Fixed an error when targeting battle pets that are participating in a pet battle ([ticket #96](https://wow.curseforge.com/projects/total-rp-3/issues/96)).
+- Fixed an issue where if you used a single space character for your class (like to indicate you have none) it would be considered as empty and your character's real class would be used instead ([ticket #103](https://wow.curseforge.com/projects/total-rp-3/issues/103)).
+- Fixed an issue where players with custom RP status from other add-ons sent via the Mary Sue Protocol would be shown as Out Of Character.
+
+### Removed
+
+- Removed workaround for the text box issue introduced in patch 7.3 as this issue has been fixed in patch 7.3.2.
 
 ]],
 		MORE_MODULES_2 = [[{h2:c}Optional modules{/h2}
