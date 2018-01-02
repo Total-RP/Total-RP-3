5dbf1324679d6e239bc2e0d27915c8c16f85533c
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-01-02 22:06:01 +0100

    WIP and fixes for #43

--- totalRP3/modules/register/main/register_cursor.lua
+++ totalRP3/modules/register/main/register_cursor.lua
@@ -5,15 +5,17 @@ local UnitIsPlayer = UnitIsPlayer;
 local UnitCanAttack = UnitCanAttack;
 local UnitVehicleSeatCount = UnitVehicleSeatCount;
 
-local hasProfile = TRP3_API.register.hasProfile;
 local isUnitIDKnown = TRP3_API.register.isUnitIDKnown;
 local openMainFrame = TRP3_API.navigation.openMainFrame;
 local openPageByUnitID = TRP3_API.register.openPageByUnitID;
 local registerConfigKey = TRP3_API.configuration.registerConfigKey;
 local getConfigValue = TRP3_API.configuration.getValue;
 local GetCursorPosition = GetCursorPosition;
-local SetCursor = SetCursor;
 local UIParent = UIParent;
+
+local function hasProfile(unit)
+	return true
+end
 
 ---Check if we can view the unit profile by using the cursor
 ---@param unitID string @ A valid unit ID (probably mouseover here)
@@ -23,7 +25,8 @@ local function canViewUnitProfile(unit)
 	not unit
 	or InCombatLockdown() -- We don't want to open stuff in combat
 	or not UnitIsPlayer(unit) -- Unit has to be a player
-	or not UnitVehicleSeatCount(unit) < 1 -- Avoid unit on multi seats mounts
+	-- TODO Check if the unit is in group
+	or not (UnitVehicleSeatCount(unit) and UnitVehicleSeatCount(unit) < 1) -- Avoid unit on multi seats mounts
 	or UnitCanAttack("player", unit) -- Unit must not be attackable
 	then
 		return false;
@@ -33,7 +36,7 @@ local function canViewUnitProfile(unit)
 	if
 	unitID == TRP3_API.globals.player_id -- Unit is not the player
 	or not isUnitIDKnown(unitID) -- Unit is known by TRP3
-	or not hasProfile(unitID) -- Unit has a RP profile available
+	or hasProfile(unitID) == nil -- Unit has a RP profile available
 	then
 		return false;
 	end
@@ -41,14 +44,17 @@ local function canViewUnitProfile(unit)
 	return true;
 end
 
-local CursorFrame = CreateFrame("Frame", nil, UIParent);
-CursorFrame:SetWidth(1);
-CursorFrame:SetHeight(1);
+---@type Frame
+local CursorFrame = TRP3_CursorFrame;
 
-CursorFrame:SetScript("OnUpdate", function()
-	local effScale, x, y = self:GetEffectiveScale(), GetCursorPosition();
-	self:ClearAllPoints();
-	self:SetPoint("CENTER", UIParent, "CENTER", (x / effScale) + 10, (y / effScale) + 10);
+CursorFrame:SetScript("OnUpdate", function(self)
+	if not UnitName("mouseover") then
+		self:Hide();
+		return
+	end
+	local scale = 1 / UIParent:GetEffectiveScale();
+	local x, y =  GetCursorPosition();
+	CursorFrame:SetPoint("CENTER", UIParent, "BOTTOMLEFT", x * scale + 33, y * scale - 30);
 end)
 
 
@@ -56,26 +62,26 @@ TRP3_API.events.listenToEvent(TRP3_API.events.WORKFLOW_ON_LOADED, function()
 
 	local CONFIG_RIGHT_CLICK_OPEN_PROFILE = "CONFIG_RIGHT_CLICK_OPEN_PROFILE";
 	registerConfigKey(CONFIG_RIGHT_CLICK_OPEN_PROFILE, true);
-
-	hooksecurefunc("InteractUnit", function(unit)
+	hooksecurefunc("TurnOrActionStart", function()
 		if not getConfigValue(CONFIG_RIGHT_CLICK_OPEN_PROFILE) then return end
-		if canViewUnitProfile(unit) then
-			local unitID = TRP3_API.utils.str.getUnitID(unit);
+		if CursorFrame:IsVisible() then
 			openMainFrame()
-			openPageByUnitID(unitID);
+			openPageByUnitID(CursorFrame.unitID);
 		end
 	end)
 
 	-- Listen to the mouse over event
-	TRP3_API.events.listenToEvent(TRP3_API.events.MOUSE_OVER_CHANGED, function(targetID, targetMode)
-		if canViewUnitProfile() then
+	TRP3_API.utils.event.registerHandler("UPDATE_MOUSEOVER_UNIT", function()
+		if canViewUnitProfile("mouseover") then
+			local unitID = TRP3_API.utils.str.getUnitID("mouseover");
+			CursorFrame.unitID = unitID;
+			-- TODO Move cursor before showing, so that it doesn't appear at its old place for one frame
 			CursorFrame:Show();
-			SetCursor([[Interface\Cursor\WorkOrders]]);
 		else
 			CursorFrame:Hide();
-			SetCursor();
 		end
 	end);
+	-- TODO Also refresh on register data update
 
 	table.insert(TRP3_API.register.CONFIG_STRUCTURE.elements, {
 		inherit = "TRP3_ConfigCheck",
