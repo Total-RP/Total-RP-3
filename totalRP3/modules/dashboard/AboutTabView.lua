3f099d3191ae92157f004fddb28f4bf253d38c4d
Daniel Yates <me@meorawr.io>
2018-03-06 22:05:51 +0000

    Build credits from tables, revamp dashboard
    This requires manual changes to the non-English locales that are
    outlined below.
    
    Modernised the dashboard whilst aiming to keep it backwards compatible
    with any existing assumptions on its structure. The codebase is
    consistent with the style being used on the color picker mixin branch,
    and is extensible to boot with features like clearly defined
    registration points for hyperlink handlers.
    
    TRP3_DashboardTabFrame is a virtual frame with a mixin attached which
    matches the old template precisely, same widgets, same sizes, same
    positioning. This is responsible for owning the tabs and switching
    between them, as well as providing the views access to the concrete
    dashboard frame itself. When instantiating this frame, you must supply
    an OnLoad handler that defines "self.TabClasses = { classes... }" and
    then call "self:OnLoad()".
    
    TRP3_DashboardHTMLContent (and its associated mixin) is a SimpleHTML
    frame template which has an overridden SetText() method to provide us
    with a GetText(), so the ".text" member is no longer needed. It also
    defines the script handlers for hyperlink management as well as a pair
    of RegisterHyperlink and UnregisterHyperlink methods. These are used by
    the "What's New" tab view to provide a clean API for adding in custom
    links for the (amazing) option toggles in the changelog.
    
    DashboardTabView is a base class for implementing the individual tab
    views. It's pretty barebones and basic, nothing special.
    
    The individual views subclass DashboardTabView and override Show to
    provide logic for updating the HTML content on the dashboard, as well
    as other stuff they need. There's also a Hide method which, oddly
    enough, is called when the tab is going to be hidden.
    
    The about view has been reworked and the credits defined there with some
    helper classes to simplify displaying names, including those special
    people who are multi-talented with loads of roles. This includes the use
    of Ellyb to get the Patreon contributors list.
    
    Manual localisation changes can be seen in the definition for the
    English (default) locale.

---
+++ totalRP3/modules/dashboard/AboutTabView.lua
@@ -0,0 +1,140 @@
+----------------------------------------------------------------------------------
+--- Total RP 3
+---
+--- Dashboard "About" Tab
+--- ------------------------------------------------------------------------------
+--- Copyright 2018 Daniel "Meorawr" Yates <me@meorawr.io>
+---
+--- Licensed under the Apache License, Version 2.0 (the "License");
+--- you may not use this file except in compliance with the License.
+--- You may obtain a copy of the License at
+---
+--- http://www.apache.org/licenses/LICENSE-2.0
+---
+--- Unless required by applicable law or agreed to in writing, software
+--- distributed under the License is distributed on an "AS IS" BASIS,
+--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--- See the License for the specific language governing permissions and
+--- limitations under the License.
+----------------------------------------------------------------------------------
+
+---@type TRP3_API
+local addonName, TRP3_API = ...;
+local Ellyb = Ellyb(addonName);
+
+-- Lua imports
+local tconcat = table.concat;
+local tinsert = table.insert;
+
+-- Ellyb imports
+local Class = Ellyb.Class;
+local Funcs = Ellyb.Funcs;
+local Strings = Ellyb.Strings;
+local Tables = Ellyb.Tables;
+
+-- Total RP 3 imports
+local Dashboard = TRP3_API.dashboard;
+local loc = TRP3_API.loc;
+local strhtml = TRP3_API.utils.str.toHTML;
+
+local Credit = Dashboard.Credit;
+local CreditRoles = Dashboard.CreditRoles;
+
+--- Mapping of replacement string keys to Credit instances that will be
+--  displayed in the replaced segment.
+local CREDITS = {
+	AUTHORS = {
+		Credit([[Renaud "{twitter*EllypseCelwe*Ellypse}" Parize]], CreditRoles.AUTHOR),
+		Credit([[Sylvain "{twitter*Telkostrasz*Telkostrasz}" Cossement]], CreditRoles.AUTHOR),
+	},
+
+	CONTRIBUTORS = {
+		ShowNamesWithRoles = true,
+
+		Credit([[{twitter*Solanya_*Solanya}]], CreditRoles.CONTRIBUTOR, CreditRoles.COMMUNITY_MANAGER),
+		Credit([[Connor "{twitter*Saelorable*Sælorable}" Macleod]], CreditRoles.CONTRIBUTOR),
+		Credit([[Daniel "Meorawr" Yates]], CreditRoles.CONTRIBUTOR),
+	},
+
+	TESTERS = {
+		Credit([[Erzan]], CreditRoles.TESTER),
+		Credit([[Calian]], CreditRoles.TESTER),
+		Credit([[Kharess]], CreditRoles.TESTER),
+		Credit([[Alnih]], CreditRoles.TESTER),
+		Credit([[611]], CreditRoles.TESTER),
+	},
+
+	GUILD_MEMBERS = {
+		Credit([[Azane]], CreditRoles.GUILD_MEMBER),
+		Credit([[Hellclaw]], CreditRoles.GUILD_MEMBER),
+		Credit([[Leylou]], CreditRoles.GUILD_MEMBER),
+	},
+};
+
+--- Returns a formatted string that contains all the names for people
+--  in a given credits section.
+--
+--- The credits table may have a "ShowNamesWithRoles" flag that controls
+--  if role information is appended to individual names.
+local function getLocalizedCreditsSection(people)
+	-- Use a temporary table and get all the localized names in the
+	-- correct format.
+	local names = Tables.getTempTable();
+	for i = 1, #people do
+		local person = people[i];
+
+		if people.ShowNamesWithRoles then
+			names[i] = person:GetLocalizedFullText();
+		else
+			names[i] = person:GetName();
+		end
+	end
+
+	-- Build a newline separated list where each line has a preceeding
+	-- list marker ("- ") using the names.
+	local formatted = ("- %s"):format(tconcat(names, "\n- "));
+
+	Tables.releaseTempTable(names);
+	return formatted;
+end
+
+--- Returns the fully formatted localized text for this view.
+local function getLocalizedText()
+	local replacements = {
+		[1] = TRP3_API.globals.version_display,
+		[2] = TRP3_API.globals.version,
+		[3] = Ellyb:GetPatreonSupporters(),
+	};
+
+	for section, people in pairs(CREDITS) do
+		replacements[section] = getLocalizedCreditsSection(people);
+	end
+
+	return strhtml(Strings.interpolate(loc("THANK_YOU_1"), replacements));
+end
+
+--- Displays the credits and some text about what addon this is, just in
+--  case the users want to tweet at the developers to tell them how loved
+--  they are.
+local AboutTabView = Class("TRP3_DashboardAboutTabView", Dashboard.TabView);
+Dashboard.AboutTabView = AboutTabView;
+
+function AboutTabView.static.getTabTitle()
+	return loc("DB_ABOUT");
+end
+
+function AboutTabView.static.getTabWidth()
+	return 175;
+end
+
+function AboutTabView:initialize(dashboard)
+	self.class.super.initialize(self, dashboard);
+
+	self.body = getLocalizedText();
+end
+
+function AboutTabView:Show()
+	self.class.super.Show(self);
+
+	self.dashboard:SetHTML(self.body);
+end
---
+++ totalRP3/modules/dashboard/Credit.lua
@@ -0,0 +1,91 @@
+----------------------------------------------------------------------------------
+--- Total RP 3
+---
+--- Dashboard Credit Class
+--- ------------------------------------------------------------------------------
+--- Copyright 2018 Daniel "Meorawr" Yates <me@meorawr.io>
+---
+--- Licensed under the Apache License, Version 2.0 (the "License");
+--- you may not use this file except in compliance with the License.
+--- You may obtain a copy of the License at
+---
+--- http://www.apache.org/licenses/LICENSE-2.0
+---
+--- Unless required by applicable law or agreed to in writing, software
+--- distributed under the License is distributed on an "AS IS" BASIS,
+--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--- See the License for the specific language governing permissions and
+--- limitations under the License.
+----------------------------------------------------------------------------------
+
+---@type TRP3_API
+local addonName, TRP3_API = ...;
+local Ellyb = Ellyb(addonName);
+
+-- Lua imports
+local tconcat = table.concat;
+
+-- Ellyb imports
+local Class = Ellyb.Class;
+local Tables = Ellyb.Tables;
+
+-- WoW imports
+local LIST_DELIMITER = LIST_DELIMITER;
+
+-- Total RP 3 imports
+local Dashboard = TRP3_API.dashboard;
+local loc = TRP3_API.loc;
+
+--- Enumeration of valid roles for use with the Credit class.
+Dashboard.CreditRoles = {
+	AUTHOR = "AUTHOR",
+	CONTRIBUTOR = "CONTRIBUTOR",
+	COMMUNITY_MANAGER = "COMMUNITY_MANAGER",
+	TESTER = "TESTER",
+	GUILD_MEMBER = "GUILD_MEMBER",
+};
+
+--- This class represents a single person to be displayed in the credits.
+local Credit = Class("TRP3_Credit");
+Dashboard.Credit = Credit;
+
+--- Initialises the credit with the given person's name string and a list of
+--  roles to assign them.
+function Credit:initialize(person, ...)
+	self.person = person;
+	self.roles = { ... };
+end
+
+--- Returns the name of the person this credit belongs to.
+function Credit:GetName()
+	return self.person;
+end
+
+--- Returns the full text to display for this role, which consists of the
+--  persons name and their role(s).
+function Credit:GetLocalizedFullText()
+	local formatString = loc("THANK_YOU_WITH_ROLE");
+	return formatString:format(self:GetName(), self:GetLocalizedRoleText());
+end
+
+--- Returns a localized string that describes the roles this person has.
+function Credit:GetLocalizedRoleText()
+	-- Zero or only one role? Simple lookup, nothing more.
+	local roleCount = #self.roles;
+	if roleCount == 0 then
+		return "";
+	elseif roleCount == 1 then
+		return loc("THANK_YOU_ROLE_" .. self.roles[1]);
+	end
+
+	-- If this person is multi-talented and can perform not one, but MULTIPLE
+	-- roles then we need to join them up with a list delimiter.
+	local localisedRoles = Tables.getTempTable();
+	for i = 1, roleCount do
+		localisedRoles[i] = loc("THANK_YOU_ROLE_" .. self.roles[i]);
+	end
+
+	local localisedRoleString = tconcat(localisedRoles, LIST_DELIMITER);
+	Tables.releaseTempTable(localisedRoles);
+	return localisedRoleString;
+end
