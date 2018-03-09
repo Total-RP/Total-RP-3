c61c0ab83aa899d116ff4a3a6fcafba69573b05f
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-03-09 10:03:35 +0100

    Use Ellyb to get list of Patreon supporters
    Fixes #103

--- totalRP3/modules/dashboard/dashboard.lua
+++ totalRP3/modules/dashboard/dashboard.lua
@@ -214,7 +214,7 @@ TRP3_API.dashboard.init = function()
 
 	moreModuleText = Utils.str.toHTML(moreModuleText);
 	whatsNewText = Utils.str.toHTML(whatsNewText);
-	aboutText = Utils.str.toHTML(aboutText:format(TRP3_API.globals.version_display, TRP3_API.globals.version, ELLYPSE_PATREON_SUPPORTERS));
+	aboutText = Utils.str.toHTML(aboutText:format(TRP3_API.globals.version_display, TRP3_API.globals.version, TRP3_API.Ellyb:GetPatreonSupporters()));
 
 	local frame = CreateFrame("Frame", "TRP3_DashboardBottomTabBar", TRP3_DashboardBottom);
 	frame:SetSize(400, 30);
