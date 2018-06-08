dcbd829f46732c54015d1ccd640c1fdd328eaf0b
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-06-08 10:19:48 +0200

    Added workaround for forbidden GameTooltip issue
    💩

--- totalRP3/tools/workarounds.lua
+++ totalRP3/tools/workarounds.lua
@@ -25,6 +25,24 @@ TRP3_API.Workarounds = Workarounds ;
 
 local workaroundsToApply = {};
 
+-- Workaround for 7.2.5 issue with debuff on friendly nameplates in dungeons that would forever forbid the GameTooltip
+-- from being accessible from add-ons.
+-- I do not like to override CVar for the user and impose options, but there is no way around it as we need to use
+-- the GameTooltip and Blizzard is giving us no choice. This situation is a fucking sad joke.
+-- See https://www.wowace.com/projects/libactionbutton-1-0/issues/23
+table.insert(workaroundsToApply, function()
+
+	-- Set the nameplateShowDebuffsOnFriendly to false, never show debuff on friendly nameplates
+	SetCVar("nameplateShowDebuffsOnFriendly", false);
+
+	-- To make sure the user never turns that option on again, we hook SetCVar and check if someone is trying to turn that on
+	hooksecurefunc("SetCVar", function(cvarName, value)
+		if  cvarName == "nameplateShowDebuffsOnFriendly" and value == true then
+			SetCVar("nameplateShowDebuffsOnFriendly", false)
+		end
+	end)
+end)
+
 function Workarounds.applyWorkarounds()
 	for _, workaround in pairs(workaroundsToApply) do
 		workaround();
