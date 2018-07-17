f9ece393b11b90a5fdddcd3e0209a2afd38b0449
Renaud "Ellypse" Parize <ellypse@totalrp3.info>
2018-07-17 21:07:01 +0200

    Remove realm name workaround, use realm name gsub

--- totalRP3/core/impl/globals.lua
+++ totalRP3/core/impl/globals.lua
@@ -125,8 +125,8 @@ setmetatable(TRP3_API.globals.empty, emptyMeta);
 --*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 TRP3_API.globals.build = function()
-	local fullName, realm = UnitFullName("player");
-	assert(realm, "Cannot have realm name information !");
+	local fullName = UnitName("player");
+	local realm = GetRealmName():gsub("[%s*%-*]", "");
 	TRP3_API.globals.player_realm_id = realm;
 	TRP3_API.globals.player_id = fullName .. "-" .. realm;
 	TRP3_API.globals.player_icon = TRP3_API.ui.misc.getUnitTexture(race, UnitSex("player"));
