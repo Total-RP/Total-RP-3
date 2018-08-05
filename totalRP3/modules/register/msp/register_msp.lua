16230af0aa9be385b5166a3b60988effcd0cc204
Justin Snelgrove <35874535+Itarater@users.noreply.github.com>
2018-08-05 15:43:25 -0400

    [modules/registe] Fix up MSP copyright line
    Per discussion in Discord. This should satisfy basically every country in the world, as far as being legally significant.

--- totalRP3/modules/register/msp/register_msp.lua
+++ totalRP3/modules/register/msp/register_msp.lua
@@ -5,7 +5,7 @@
 ---	---------------------------------------------------------------------------
 ---	Copyright 2014 Sylvain Cossement (telkostrasz@telkostrasz.be)
 --- Copyright 2018 Renaud "Ellypse" Parize <ellypse@totalrp3.info> @EllypseCelwe
---- © Justin Snelgrove
+--- Copyright / © 2018 Justin Snelgrove
 ---
 ---	Licensed under the Apache License, Version 2.0 (the "License");
 ---	you may not use this file except in compliance with the License.
