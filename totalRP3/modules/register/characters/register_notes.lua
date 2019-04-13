bb67582e523271a9ae97f556bc8fd9f493cfdf4a
Solanya <paul.corlay@gmail.com>
2019-04-13 14:03:23 +0200

    Added account-wide notes logic

--- totalRP3/modules/register/characters/register_notes.lua
+++ totalRP3/modules/register/characters/register_notes.lua
@@ -28,19 +28,54 @@ local hasProfile = TRP3_API.register.hasProfile;
 local openMainFrame = TRP3_API.navigation.openMainFrame;
 local getCurrentContext = TRP3_API.navigation.page.getCurrentContext;
 local setupIconButton = TRP3_API.ui.frame.setupIconButton;
+
+local GetCurrentUser = AddOn_TotalRP3.Player.GetCurrentUser;
 local getPlayerCurrentProfileID = TRP3_API.profile.getPlayerCurrentProfileID;
 
 local NOTES_ICON = Ellyb.Icon("Inv_misc_scrollunrolled03b");
 
+local function displayNotes(context)
+
+    local profileID = context.profileID;
+    if context.isPlayer then
+        profileID = getPlayerCurrentProfileID();
+    end
+
+    assert(profileID, "No profileID in context !");
+
+    local profileNotes = GetCurrentUser():GetProfile().notes;
+    TRP3_RegisterNotesViewProfileScrollText:SetText(profileNotes and profileNotes[profileID] or "");
+    TRP3_RegisterNotesViewAccountScrollText:SetText(TRP3_Notes and TRP3_Notes[profileID] or "");
+end
+
+local function onAccountNotesChanged()
+    local context = getCurrentContext();
+    local profileID = context.profileID;
+    if context.isPlayer then
+        profileID = getPlayerCurrentProfileID();
+    end
+
+    TRP3_Notes[profileID] = TRP3_RegisterNotesViewAccountScrollText:GetText();
+end
+
 local function showNotesTab()
     local context = getCurrentContext();
     assert(context, "No context for page player_main !");
+    assert(context.profile, "No profile in context");
     TRP3_ProfileReportButton:Hide();
     TRP3_RegisterNotes:Show();
+    displayNotes(context);
 end
 TRP3_API.register.ui.showNotesTab = showNotesTab;
 
 function TRP3_API.register.inits.notesInit()
+
+    if not TRP3_Notes then
+        TRP3_Notes = {};
+    end
+
+    TRP3_RegisterNotesViewAccountScrollText:SetScript("OnTextChanged", onAccountNotesChanged);
+
     TRP3_API.Events.registerCallback(TRP3_API.Events.WORKFLOW_ON_LOADED, function()
         local openPageByUnitID = TRP3_API.register.openPageByUnitID;
         local openNotesTab = TRP3_TabBar_Tab_5:GetScript("OnClick");    -- This was a quick workaround for RP.IO, is there a better option ?
