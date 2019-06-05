471e4419071966b4161a4569bae71df905932f14
Solanya <paul.corlay@gmail.com>
2019-06-05 22:54:54 +0200

    Fixed map scan button anchor

--- totalRP3/modules/map/WorldMapButton.lua
+++ totalRP3/modules/map/WorldMapButton.lua
@@ -24,6 +24,7 @@ local Ellyb = TRP3_API.Ellyb;
 local loc = TRP3_API.loc;
 local Events = TRP3_API.Events;
 local getConfigValue = TRP3_API.configuration.getValue;
+local setConfigValue = TRP3_API.configuration.setValue;
 local registerConfigKey = TRP3_API.configuration.registerConfigKey;
 local displayDropDown = TRP3_API.ui.listbox.displayDropDown;
 --endregion
@@ -48,8 +49,9 @@ local ON_COOLDOWN_STATE_MAP_ICON = Ellyb.Icon("ability_mage_timewarp")
 Events.registerCallback(Events.WORKFLOW_ON_LOADED, function()
 	registerConfigKey(CONFIG_MAP_BUTTON_POSITION, "BOTTOMLEFT");
 
-	local function placeMapButton()
-		local position = getConfigValue(CONFIG_MAP_BUTTON_POSITION)
+	local function placeMapButton(newPosition)
+		if newPosition then setConfigValue(newPosition) end
+		local position = newPosition or getConfigValue(CONFIG_MAP_BUTTON_POSITION)
 
 		WorldMapButton:SetParent(WorldMapFrame.BorderFrame);
 		WorldMapButton:ClearAllPoints();
