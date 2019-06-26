87cb2d5b502cbc09e0fd12e4206b6c0d94559b5f
Daniel Yates <me@meorawr.io>
2019-06-26 01:50:26 +0100

    Don't pass nil MU to path => ID conversion func
    The value received via MSP for the MU field gets passed through
    emptyToNil, so in the event we get an empty MU field back we'd
    be passing a nil through to LibRPMedia's conversion function.

--- totalRP3/modules/register/msp/register_msp.lua
+++ totalRP3/modules/register/msp/register_msp.lua
@@ -340,7 +340,12 @@ local function onStart()
 						end
 					elseif ABOUT_FIELDS[field] then
 						if field == "MU" then
-							profile.about.MU = tonumber(value) or Utils.music.convertPathToID(value);
+							local fileID = tonumber(value);
+							if not fileID and type(value) == "string" then
+								fileID = Utils.music.convertPathToID(value);
+							end
+
+							profile.about.MU = fileID;
 						else
 							local old;
 							if profile.about.T3 and profile.about.T3[ABOUT_FIELDS[field]] then
