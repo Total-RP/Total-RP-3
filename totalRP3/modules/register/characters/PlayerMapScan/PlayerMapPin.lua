66e1bc1855ab3c7b72cfc2b1e597a0c17e2350a1
Daniel Yates <me@meorawr.io>
2019-07-21 19:49:50 +0100

    Protect against Extended's bad decisions

--- totalRP3/modules/register/characters/PlayerMapScan/PlayerMapPin.lua
+++ totalRP3/modules/register/characters/PlayerMapScan/PlayerMapPin.lua
@@ -112,7 +112,7 @@ function TRP3_PlayerMapPinMixin:Decorate(displayData)
 	-- Adjust the frame level of pins that have a zero or higher priority.
 	-- This applies to pins that have a relationship status set.
 	local priority = displayData.categoryPriority or -1;
-	if priority >= 0 then
+	if type(priority) == "number" and priority >= 0 then
 		-- Topmost level if there's any sort of priority.
 		self:UseFrameLevelType("PIN_FRAME_LEVEL_TOPMOST");
 	else
