8aea1ff69b6ad51aee6485d4bc2248d59948af17
Daniel Yates <me@meorawr.io>
2019-07-21 19:53:34 +0100

    Show player POIs with relations above others (#331)
    * Show player POIs with relations above others
    
    We'll use the topmost framelevel as there's not too many other
    supported options (short of registering our own).
    
    * Default priority test, check if > 0
    
    This accounts for war mode pins having a priority (even if -1) from
    being shown above players that have, err, no priority.
    
    Logic.
    
    * Fix priority test for Family relations
    
    * Protect against Extended's bad decisions

--- totalRP3/modules/register/characters/PlayerMapScan/PlayerMapPin.lua
+++ totalRP3/modules/register/characters/PlayerMapScan/PlayerMapPin.lua
@@ -109,6 +109,17 @@ function TRP3_PlayerMapPinMixin:Decorate(displayData)
 		self.Texture:SetVertexColor(1, 1, 1, 1);
 	end
 
+	-- Adjust the frame level of pins that have a zero or higher priority.
+	-- This applies to pins that have a relationship status set.
+	local priority = displayData.categoryPriority or -1;
+	if type(priority) == "number" and priority >= 0 then
+		-- Topmost level if there's any sort of priority.
+		self:UseFrameLevelType("PIN_FRAME_LEVEL_TOPMOST");
+	else
+		-- Default level.
+		self:UseFrameLevelType("PIN_FRAME_LEVEL_VEHICLE_ABOVE_GROUP_MEMBER");
+	end
+
 	self.Texture:SetAlpha(displayData.opacity or 1)
 
 	Ellyb.Tooltips.getTooltip(self):SetTitle(ORANGE(loc.REG_PLAYERS)):ClearLines();
