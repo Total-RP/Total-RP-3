7b81534c4d17f5fd4bbeec0fddb93aeb2d23dbf0
Solanya <paul.corlay@gmail.com>
2019-12-12 23:00:19 +0100

    Updated release.sh Classic version

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -s https://raw.githubusercontent.com/Total-RP/packager/master/release.sh | bash -s -- -p 75973 -w 24113 -g 8.2.5 -o
-curl -s https://raw.githubusercontent.com/Total-RP/packager/master/release.sh | bash -s -- -p 335857 -w 25153 -g 1.13.2 -o
+curl -s https://raw.githubusercontent.com/Total-RP/packager/master/release.sh | bash -s -- -p 335857 -w 25153 -g 1.13.3 -o
