09366a641b93004c77fe847de5e3bc9d05b72220
Ellypse <ellypse@totalrp3.info>
2020-06-09 17:42:14 +0200

    Update naming to "blocklist" and "safelist"
    Replaced occurrences of the terms "blacklist" and "whitelist" in the code and in the add-ons localization in favor of the terms "blocklist" and "safelist".

--- totalRP3/modules/register/characters/register_main.lua
+++ totalRP3/modules/register/characters/register_main.lua
@@ -197,7 +197,7 @@ local function unitIDIsFilteredForMatureContent(unitID)
 	local profileID = getUnitIDProfileID(unitID);
 	-- Check if the profile has been flagged as containing mature content, that the option to filter such content is enabled
 	-- and that the profile is not in the pink list.
-	return profile.hasMatureContent and getConfigValue("register_mature_filter") and not (TRP3_API.register.mature_filter.isProfileWhitelisted(profileID))
+	return profile.hasMatureContent and getConfigValue("register_mature_filter") and not (TRP3_API.register.mature_filter.isProfileSafeListed(profileID))
 end
 
 TRP3_API.register.unitIDIsFilteredForMatureContent = unitIDIsFilteredForMatureContent;
@@ -207,7 +207,7 @@ local function profileIDISFilteredForMatureContent (profileID)
 
 	local profile = getProfileOrNil(profileID);
 
-	return profile and profile.hasMatureContent and not TRP3_API.register.mature_filter.isProfileWhitelisted(profileID);
+	return profile and profile.hasMatureContent and not TRP3_API.register.mature_filter.isProfileSafeListed(profileID);
 end
 
 TRP3_API.register.profileIDISFilteredForMatureContent = profileIDISFilteredForMatureContent;
