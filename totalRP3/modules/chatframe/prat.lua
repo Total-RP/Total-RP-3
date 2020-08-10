3ce1422bb7e1dfb78fa07fa0863a1f1ecee04d1d
Solanya <17127080+Solanya@users.noreply.github.com>
2020-08-10 15:40:06 +0200

    Added safeguard for WIM

--- totalRP3/modules/chatframe/prat.lua
+++ totalRP3/modules/chatframe/prat.lua
@@ -77,8 +77,8 @@ local function onStart()
 
 			if disabledByOOC() then return end;
 
-			-- If the message has no GUID (system?) we don't have anything to do with this
-			if not message.GUID then return end;
+			-- If the message has no GUID (system?) or an invalid GUID (WIM >:( ) we don't have anything to do with this
+			if not message.GUID or not C_PlayerInfo.GUIDIsPlayer(message.GUID) then return end;
 
 			-- Do not do any modification if the channel is not handled by TRP3 or customizations has been disabled
 			-- for that channel in the settings
