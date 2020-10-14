852675535ba622cad9965ce74e5c8c93b734f65e
Daniel Yates <me@meorawr.io>
2020-10-14 22:18:41 +0100

    Improve handling of improper case in /trp3 open
    Before, we'd only attempt to uppercase the first letter of the name and
    wouldn't try to case-correct the realm. Similarly upon receiving new
    register data we would check if the sender (which will be case-correct)
    against a possibly-incorrect user input, and if these didn't match the
    UI wouldn't open.
    
    The new logic fixes the above issues by attempting to case-correct both
    the requested player name and their realm separately. If the user still
    manages to screw this up (by typing the whole thing in caps), then when
    register data is updated we'll now do a case-insensitive comparison
    against the sender and open the UI based on that identifier rather than
    the user input.

--- totalRP3/modules/register/main/register_exchange.lua
+++ totalRP3/modules/register/main/register_exchange.lua
@@ -497,9 +497,21 @@ TRP3_API.slash.registerCommand({
 				-- If we typed a unit token we resolve it
 				characterToOpen = Utils.str.getUnitID(characterToOpen:lower());
 			else
-				-- Capitalizing first letter of the name, just in case someone is lazy.
-				-- We don't have a solution for "I'm lazy but need someone from another realm" yet.
-				characterToOpen = characterToOpen:gsub("^%l", string.upper);
+				-- Capitalizing first letter of the name/realm, just in case someone is lazy.
+
+				local name, realm = AddOn_Chomp.NameSplitRealm(characterToOpen);
+
+				if not name then
+					displayMessage(loc.PR_SLASH_OPEN_EXAMPLE);
+					return;
+				end
+
+				realm = realm or TRP3_API.globals.player_realm_id;
+
+				name  = string.gsub(name, "^%l", string.upper);
+				realm = string.gsub(realm, "^%l", string.upper);
+
+				characterToOpen = AddOn_Chomp.NameMergedRealm(name, realm);
 			end
 
 			-- If no realm has been entered, we use the player's realm automatically
@@ -537,9 +549,14 @@ TRP3_API.slash.registerCommand({
 
 -- Event for the "/trp3 open" command
 Events.listenToEvent(Events.REGISTER_DATA_UPDATED, function(unitID, _, dataType)
-	if unitID == characterToOpen and (not dataType or dataType == "character") then
+	if AddOn_Chomp.InsensitiveStringEquals(characterToOpen, unitID)
+		and (not dataType or dataType == "character") then
+		-- Use the unitID when opening the UI as it will be precisely what's
+		-- in the register, whereas characterToOpen might have incorrect
+		-- casing if, for example, the user typed the name in all-caps.
+
 		TRP3_API.navigation.openMainFrame();
-		TRP3_API.register.openPageByUnitID(characterToOpen);
+		TRP3_API.register.openPageByUnitID(unitID);
 		if commandOpeningTimerHandle then
 			commandOpeningTimerHandle:Cancel();
 		end
