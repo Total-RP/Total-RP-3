11d4d7423e9a9f26ca9c4a15075d4fd5544fc12c
Solanya <17127080+Solanya@users.noreply.github.com>
2020-10-21 11:32:28 +0200

    Better format #2

--- totalRP3/core/impl/slash.lua
+++ totalRP3/core/impl/slash.lua
@@ -181,7 +181,7 @@ TRP3_API.events.listenToEvent(TRP3_API.events.WORKFLOW_ON_LOADED, function()
 		if sender ~= Globals.player_id then
 			if type(arg) == "table" then
 				if arg.c and arg.d and arg.t then
-					local modifierString = (arg.m ~= 0) and ((arg.m > 0 and '+')..(arg.m)) or ''; -- we add a + to positive modifiers and don't render a 0 value
+					local modifierString = (arg.m == 0) and "" or format("%+d", arg.m); -- we add a + to positive modifiers and don't render a 0 value
 					Utils.message.displayMessage(loc.DICE_ROLL_T:format(Utils.str.icon(TRP3_API.globals.is_classic and "inv_enchant_shardglowingsmall" or "inv_misc_dice_02", 20), sender, arg.c, arg.d, modifierString, arg.t));
 				elseif arg.t then
 					local totalMessage = loc.DICE_TOTAL_T:format(Utils.str.icon(TRP3_API.globals.is_classic and "inv_enchant_shardglowingsmall" or "inv_misc_dice_01", 20), sender, arg.t);
