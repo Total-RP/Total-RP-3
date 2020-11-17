f4446c0c2dfc5fac05b601890040ef4c3dd9ec38
Solanya <paul.corlay@gmail.com>
2020-11-17 20:55:06 +0100

    Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## Added
 
+- Added 113 images, 1437 icons and 613 musics from patch 9.0.
 - Added modifier to the dice roll output text.
 - Added keybinding options for opening the target profile and toggling RP status.
 
@@ -10,6 +11,7 @@
 - Fixed an issue with the default tooltip appearing sometimes despite "Hide original tooltip" setting being checked.
 - Fixed an issue with the TRP3 tooltip being offset while the default tooltip is hidden.
 - Fixed an issue with the TRP3 tooltip briefly overlapping the default tooltip if quickly hovering from a player to a spell/item.
+- Fixed an issue when trying to open the Prat modules window while TRP3 is running.
 - Fixed a display issue with the NPC speech frame background.
 - Fixed alphabetical sorting when hovering a cluster of players in the map scan.
 - Potential fix for the guide channel being swapped with the xtensionxtooltip2 channel.
--- totalRP3/tools/Locale.lua
+++ totalRP3/tools/Locale.lua
@@ -1438,6 +1438,7 @@ We are aware of a current issue on Retail causing **quest item usage from the ob
 
 ## Added
 
+- Added 113 images, 1437 icons and 613 musics from patch 9.0.
 - Added modifier to the dice roll output text.
 - Added keybinding options for opening the target profile and toggling RP status.
 
@@ -1446,6 +1447,7 @@ We are aware of a current issue on Retail causing **quest item usage from the ob
 - Fixed an issue with the default tooltip appearing sometimes despite "Hide original tooltip" setting being checked.
 - Fixed an issue with the TRP3 tooltip being offset while the default tooltip is hidden.
 - Fixed an issue with the TRP3 tooltip briefly overlapping the default tooltip if quickly hovering from a player to a spell/item.
+- Fixed an issue when trying to open the Prat modules window while TRP3 is running.
 - Fixed a display issue with the NPC speech frame background.
 - Fixed alphabetical sorting when hovering a cluster of players in the map scan.
 - Potential fix for the guide channel being swapped with the xtensionxtooltip2 channel.
