0526230d45015f6fd04a9cebe4c96c3aaad142e5
Daniel Yates <me@meorawr.io>
2022-11-13 15:46:04 +0000

    Restrict the flow of information to the masses
    This removes the integration that a few systems had with the API
    documentation addon. Reasoning is as follows:
    
      1. We're not using this documentation at all.
      2. We're not maintaining this documentation for accuracy.
      3. We're not routinely adding new documentation to the system.
    
    As we're not really using it I'm not seeing much purpose in keeping
    it around and hogging up static memory for something that almost
    no user is ever going to use.

--- totalRP3/Core/Compression.lua
+++ totalRP3/Core/Compression.lua
@@ -45,37 +45,3 @@ function Compression.decompress(compressedData, wasReceivedViaAddOnChannel)
 end
 
 AddOn_TotalRP3.Compression = Compression;
-
--- Documentation
-Ellyb.Documentation:AddDocumentationTable("TotalRP3.Compression", {
-	Name = "TotalRP3.Compression",
-	Type = "System",
-	Namespace = "AddOn_TotalRP3.Compression",
-
-	Functions = {
-		{
-			Name = "compress",
-			Type = "Function",
-			Documentation = { "Compress a string." },
-			Arguments = {
-				{ Name = "data", Type = "string", Nilable = false, Documentation = { "The data we want to compress" } },
-				{ Name = "willBeSentViaAddOnChannel", Type = "boolean", Nilable = true, Documentation = { "Set to true if the data is meant to be sent via addon channels. Some special characters not suitable for communication channels will be escaped." } },
-			},
-			Returns = {
-				{ Name = "compressedData", Type = "sring", Nilable = false, Documentation = { "The compressed data." } },
-			},
-		},
-		{
-			Name = "decompress",
-			Type = "Function",
-			Documentation = { "Decompress a previously compressed string." .. ORANGE("\nWill throw an error if the algorithm was not able to decompress the data.") },
-			Arguments = {
-				{ Name = "compressedData", Type = "string", Nilable = false, Documentation = { "The data that was compressed and that we want to decompress" } },
-				{ Name = "wasReceivedViaAddOnChannel", Type = "boolean", Nilable = true, Documentation = { "Set to true if the data was received via addon channels. Some special characters have been escaped and need to be unescaped before the data can be decompressed." } },
-			},
-			Returns = {
-				{ Name = "data", Type = "sring", Nilable = false, Documentation = { "The decompressed data." } },
-			},
-		},
-	},
-});
