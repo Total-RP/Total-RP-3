0b04cd1dafaf1e7b6244f1a2ec8eef5fa3098daa
Ghost - Ben <Bcaltons5@gmail.com>
2023-05-04 23:20:37 -0500

    Cleanup

--- totalRP3/Modules/NamePlates/NamePlates_Plater.lua
+++ totalRP3/Modules/NamePlates/NamePlates_Plater.lua
@@ -86,9 +86,7 @@ function TRP3_PlaterNamePlates:CustomizeNameplate(nameplate, unitToken, displayI
 
 	-- Update nameplate visibility
 	-- We need to check if the nameplate is still visible first, so we don't re-show it if it's been hidden elsewhere
-	if unitFrame:IsShown() then
-		unitFrame:SetShown(not displayInfo.shouldHide);
-	end
+	unitFrame:SetShown(not displayInfo.shouldHide);
 
 	-- Set the color of the name to the RP profile color
 	if displayInfo.shouldColorName then
