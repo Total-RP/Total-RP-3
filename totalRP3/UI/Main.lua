c81bde4241c79b953a28ee9076fd8f22682fc81e
Daniel Yates <me@meorawr.io>
2023-11-07 23:50:36 +0000

    Patch/unheck extended and its stupid choices (#807)
    * Isolate layout to separate mixin
    
    * Correctly forward OnSizeChanged

--- totalRP3/UI/Main.lua
+++ totalRP3/UI/Main.lua
@@ -15,19 +15,10 @@ TRP3_MainFrameMixin = {};
 
 function TRP3_MainFrameMixin:OnLoad()
 	self.windowState = WindowState.Normal;
-	self.windowLayout = nil;  -- Aliases configuration table; set during addon load.
 
 	tinsert(UISpecialFrames, self:GetName());
 	TRP3_Addon.RegisterCallback(self, "CONFIGURATION_CHANGED", "OnConfigurationChanged");
-	TRP3_Addon.RegisterCallback(self, "WORKFLOW_ON_FINISH", "OnLayoutLoaded");
 	TRP3_API.ui.frame.initResize(self.Resize);
-end
-
-function TRP3_MainFrameMixin:OnLayoutLoaded()
-	self.windowLayout = TRP3_API.configuration.getValue("window_layout");
-	LibWindow.RegisterConfig(self, self.windowLayout);
-	LibWindow.MakeDraggable(self);
-	self:RestoreLayout();
 end
 
 function TRP3_MainFrameMixin:OnConfigurationChanged(_, key)
@@ -41,10 +32,6 @@ function TRP3_MainFrameMixin:OnShow()
 end
 
 function TRP3_MainFrameMixin:OnSizeChanged()
-	if self:IsLayoutLoaded() then
-		self:SaveLayout();
-	end
-
 	TRP3_Addon:TriggerEvent(TRP3_Addon.Events.NAVIGATION_RESIZED, TRP3_MainFramePageContainer:GetSize());
 end
 
@@ -65,28 +52,6 @@ end
 function TRP3_MainFrameMixin:ResizeWindow(width, height)
 	self:SetSize(width, height);
 	self:SetWindowState(WindowState.Normal);
-end
-
-function TRP3_MainFrameMixin:IsLayoutLoaded()
-	return self.windowLayout ~= nil;
-end
-
-function TRP3_MainFrameMixin:RestoreLayout()
-	assert(self:IsLayoutLoaded(), "attempted to restore window layout before layout has been loaded");
-
-	local width = self.windowLayout.w or DEFAULT_WINDOW_WIDTH;
-	local height = self.windowLayout.h or DEFAULT_WINDOW_HEIGHT;
-	self:SetSize(width, height);
-	LibWindow.RestorePosition(self);
-end
-
-function TRP3_MainFrameMixin:SaveLayout()
-	assert(self:IsLayoutLoaded(), "attempted to save window layout before layout has been loaded");
-
-	local width, height = self:GetSize();
-	self.windowLayout.w = width;
-	self.windowLayout.h = height;
-	LibWindow.SavePosition(self);
 end
 
 function TRP3_MainFrameMixin:GetWindowState()
@@ -116,4 +81,50 @@ function TRP3_MainFrameMixin:UpdateWindowStateButtons()
 
 	self.Minimize:SetShown(state == WindowState.Maximized and ShouldShowWindowStateButtons());
 	self.Maximize:SetShown(state == WindowState.Normal and ShouldShowWindowStateButtons());
+end
+
+TRP3_MainFrameLayoutMixin = CreateFromMixins(TRP3_MainFrameMixin);
+
+function TRP3_MainFrameLayoutMixin:OnLoad()
+	TRP3_MainFrameMixin.OnLoad(self);
+
+	self.windowLayout = nil;  -- Aliases configuration table; set during addon load.
+	TRP3_Addon.RegisterCallback(self, "WORKFLOW_ON_FINISH", "OnLayoutLoaded");
+end
+
+function TRP3_MainFrameLayoutMixin:OnLayoutLoaded()
+	self.windowLayout = TRP3_API.configuration.getValue("window_layout");
+	LibWindow.RegisterConfig(self, self.windowLayout);
+	LibWindow.MakeDraggable(self);
+	self:RestoreLayout();
+end
+
+function TRP3_MainFrameLayoutMixin:OnSizeChanged(...)
+	if self:IsLayoutLoaded() then
+		self:SaveLayout();
+	end
+
+	TRP3_MainFrameMixin.OnSizeChanged(self, ...);
+end
+
+function TRP3_MainFrameLayoutMixin:IsLayoutLoaded()
+	return self.windowLayout ~= nil;
+end
+
+function TRP3_MainFrameLayoutMixin:RestoreLayout()
+	assert(self:IsLayoutLoaded(), "attempted to restore window layout before layout has been loaded");
+
+	local width = self.windowLayout.w or DEFAULT_WINDOW_WIDTH;
+	local height = self.windowLayout.h or DEFAULT_WINDOW_HEIGHT;
+	self:SetSize(width, height);
+	LibWindow.RestorePosition(self);
+end
+
+function TRP3_MainFrameLayoutMixin:SaveLayout()
+	assert(self:IsLayoutLoaded(), "attempted to save window layout before layout has been loaded");
+
+	local width, height = self:GetSize();
+	self.windowLayout.w = width;
+	self.windowLayout.h = height;
+	LibWindow.SavePosition(self);
 end
