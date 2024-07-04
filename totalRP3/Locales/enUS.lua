f6e4889d9b886a153a5a4dc5e37e8289f699a559
Daniel Yates <me@meorawr.io>
2024-07-04 01:19:16 +0100

    you're breaking my heart rae

--- totalRP3/Locales/enUS.lua
+++ totalRP3/Locales/enUS.lua
@@ -797,13 +797,13 @@ Use the |cffffff00Import profile|r option to paste data from a previous export i
 	DB_STATUS_RP_OOC_TT = "You are out of your character.\nYour actions can't be associated to him/her.",
 	DB_STATUS_XP = "Roleplay proficiency",
 	DB_STATUS_XP_NEWCOMER = "Newcomer",
-	DB_STATUS_XP_NEWCOMER_TT = "This selection will indicate to other players indicating to others that you are a beginner roleplayer.|n|nThis will be shown in an icon on your tooltip.",
+	DB_STATUS_XP_NEWCOMER_TT = "This selection will indicate to other players that you are a beginner roleplayer.|n|nThis will be shown as an icon on your tooltip.",
 	DB_STATUS_XP_NORMAL = "Casual",
 	DB_STATUS_XP_NORMAL_TT = "This selection will indicate to other players that you are a casual roleplayer with no specific experience level.|n|nThis will not show any specific icon on your tooltip.",
 	DB_STATUS_XP_VETERAN = "Veteran",
 	DB_STATUS_XP_VETERAN_TT = "This selection will indicate to other players that you consider yourself to be an experienced roleplayer.|n|nThis will not show any specific icon on your tooltip.",
 	DB_STATUS_XP_NEWCOMER_GUIDE = "Newcomer Guide",
-	DB_STATUS_XP_NEWCOMER_GUIDE_TT = "This selection indicate to beginner roleplayers that you are willing to help them.|n|nThis will be shown in an icon on your tooltip.",
+	DB_STATUS_XP_NEWCOMER_GUIDE_TT = "This selection indicate to beginner roleplayers that you are willing to help them.|n|nThis will be shown as an icon on your tooltip.",
 
 	DB_STATUS_LC = "Roleplay language",
 	DB_STATUS_LC_TT = [[Sets your preferred roleplaying language. This will be shared with other compatible RP addon users.
