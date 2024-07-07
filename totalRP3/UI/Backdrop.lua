b39525b98f52bc04b514f2eff4ee77e44c5ba9ee
Daniel Yates <me@meorawr.io>
2024-07-07 15:28:31 +0100

    Make relations page pretty

--- totalRP3/UI/Backdrop.lua
+++ totalRP3/UI/Backdrop.lua
@@ -5,6 +5,15 @@ TRP3_BACKDROP_COLOR_DARK_GREY = TRP3_API.CreateColor(0.4, 0.4, 0.4);
 TRP3_BACKDROP_COLOR_GOLD = TRP3_API.CreateColor(1, 0.675, 0.125);
 TRP3_BACKDROP_COLOR_GREY = TRP3_API.CreateColor(0.6, 0.6, 0.6);
 TRP3_BACKDROP_COLOR_LIGHT_GREY = TRP3_API.CreateColor(0.8, 0.8, 0.8);
+TRP3_BACKDROP_COLOR_CREAMY_BROWN = TRP3_API.CreateColor(0.48, 0.39, 0.32);
+
+TRP3_BACKDROP_OVERLAY = {
+	bgFile   = "Interface\\DialogFrame\\UI-DialogBox-Background",
+	tile     = true,
+	tileEdge = true,
+	tileSize = 32,
+	insets   = { left = 4, right = 4, top = 4, bottom = 4 },
+};
 
 TRP3_BACKDROP_DIALOG_TOOLTIP = {
 	bgFile   = "Interface\\DialogFrame\\UI-DialogBox-Background",
