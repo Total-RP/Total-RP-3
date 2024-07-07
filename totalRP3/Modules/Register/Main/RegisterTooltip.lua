dddff8c4f74eac6d7798ffdbc07326bae0c3c70b
Raenore <172234435+Raenore@users.noreply.github.com>
2024-07-07 21:52:30 +0200

    Use quest ! for unread instead of custom

--- totalRP3/Modules/Register/Main/RegisterTooltip.lua
+++ totalRP3/Modules/Register/Main/RegisterTooltip.lua
@@ -31,8 +31,8 @@ local TRP3_Enums = AddOn_TotalRP3.Enums;
 local OOC_ICON = "|TInterface\\COMMON\\Indicator-Red:15:15|t";
 local ALLIANCE_ICON = "|TInterface\\GROUPFRAME\\UI-Group-PVP-Alliance:20:20|t";
 local HORDE_ICON = "|TInterface\\GROUPFRAME\\UI-Group-PVP-Horde:20:20|t";
-local NEW_ABOUT_ICON = "|TInterface\\AddOns\\totalRP3\\Resources\\UI\\ui-indicator-unread-about:24:24|t";
-local PROFILE_NOTES_ICON = "|TInterface\\Buttons\\UI-GuildButton-PublicNote-Up:24:24|t";
+local NEW_ABOUT_ICON = "|A:QuestNormal:22:22|a";
+local PROFILE_NOTES_ICON = "|TInterface\\Buttons\\UI-GuildButton-PublicNote-Up:22:22|t";
 local TRANSPARENT_ICON = "|TInterface\\AddOns\\totalRP3\\Resources\\UI\\transparent:24:24|t";
 
 local ConfigKeys = {
