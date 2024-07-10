d28982ac96fe7cd0a12deb07c384cfdd90affd1e
Daniel Yates <me@meorawr.io>
2024-07-10 22:21:36 +0100

    Remove errant space

--- totalRP3/Modules/Dashboard/Dashboard.lua
+++ totalRP3/Modules/Dashboard/Dashboard.lua
@@ -35,7 +35,7 @@ local get, getDefaultProfile = TRP3_API.profile.getData, TRP3_API.profile.getDef
 getDefaultProfile().player.character = {
 	v = 1,
 	RP = TRP3_Enums.ROLEPLAY_STATUS.OUT_OF_CHARACTER,
-	WU =  TRP3_Enums.WALKUP.NO,
+	WU = TRP3_Enums.WALKUP.NO,
 }
 
 local function incrementCharacterVernum()
