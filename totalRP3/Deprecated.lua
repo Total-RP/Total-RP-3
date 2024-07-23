ac7a3f67e2af586853d6f3a2a44ed7d9f3f10756
Daniel Yates <me@meorawr.io>
2024-07-23 17:42:39 +0100

    Murder the FieldSetFrame template (#1091)
    Extended cleaned up all of its usages of the template, so no longer
    required yay poggers let's go boys.

--- totalRP3/Deprecated.lua
+++ totalRP3/Deprecated.lua
@@ -3,14 +3,3 @@
 
 -- Anything in this file is deprecated and will be removed in future versions
 -- of TRP, potentially without warning.
-
-local FIELDSET_DEFAULT_CAPTION_WIDTH = 100;
-
-function TRP3_API.ui.frame.setupFieldPanel(fieldset, text, size)
-	if fieldset and _G[fieldset:GetName().."CaptionPanelCaption"] then
-		_G[fieldset:GetName().."CaptionPanelCaption"]:SetText(text);
-		if _G[fieldset:GetName().."CaptionPanel"] then
-			_G[fieldset:GetName().."CaptionPanel"]:SetWidth(size or FIELDSET_DEFAULT_CAPTION_WIDTH);
-		end
-	end
-end
