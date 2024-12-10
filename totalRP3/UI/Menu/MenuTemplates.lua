7bf24c5d03b8a1fb250a64eab4ef305afdeb8369
Daniel Yates <me@meorawr.io>
2024-12-10 17:35:22 +0000

    Remove all old dropdown compatibility code
    As modern menus are now supported in all client flavours, our
    compatibility layer for new menu APIs and UIDropDownMenu driven menus is
    no longer required.
    
    The only things left for the compatibility layer are the dropdown button
    template itself, and the one utility function for opening a context
    menu. These are used too widely in the codebase at present and I don't
    want to unnecessarily replace these until a future expansion patch.

--- totalRP3/UI/Menu/MenuTemplates.lua
+++ totalRP3/UI/Menu/MenuTemplates.lua
@@ -1,146 +1,20 @@
 -- Copyright The Total RP 3 Authors
 -- SPDX-License-Identifier: Apache-2.0
 
-local function CreateTextElementDescription(text)
-	local elementDescription = TRP3_Menu.CreateMenuElementDescription();
-	elementDescription.text = text;
-	return elementDescription;
-end
-
-local function CreateButtonDescription(text)
-	local function Initializer(info, description, menu)  -- luacheck: no unused
-		info.text = TRP3_MenuUtil.GetElementText(description);
-		info.disabled = not description:IsEnabled();
-		info.hasArrow = description:CanOpenSubmenu();
-	end
-
-	local elementDescription = CreateTextElementDescription(text);
-	elementDescription:AddInitializer(Initializer);
-	return elementDescription;
-end
-
-local function CreateCheckboxDescription(text)
-	local function Initializer(info, description, menu)  -- luacheck: no unused
-		info.checked = function() return description:IsSelected(); end;
-		info.isNotRadio = true;
-		info.notCheckable = false;
-	end
-
-	local elementDescription = CreateButtonDescription(text);
-	elementDescription:AddInitializer(Initializer);
-	return elementDescription;
-end
-
-local function CreateRadioDescription(text)
-	local function Initializer(info, description, menu)  -- luacheck: no unused
-		info.checked = function() return description:IsSelected(); end;
-		info.notCheckable = false;
-	end
-
-	local elementDescription = CreateButtonDescription(text);
-	elementDescription:AddInitializer(Initializer);
-	return elementDescription;
-end
-
-local function CreateTitleDescription(text, color)
-	local function Initializer(info, description, menu)  -- luacheck: no unused
-		info.colorCode = color and color:GenerateHexColorMarkup() or nil;
-		info.isTitle = true;
-	end
-
-	local elementDescription = CreateButtonDescription(text);
-	elementDescription:AddInitializer(Initializer);
-	return elementDescription;
-end
-
-local function CreateDividerDescription()
-	local function Initializer(info, description, menu)  -- luacheck: no unused
-		info.hasArrow = false;
-		info.icon = "Interface\\Common\\UI-TooltipDivider-Transparent";
-		info.iconInfo = { tCoordLeft = 0, tCoordRight = 1, tCoordTop = 0, tCoordBottom = 1, tSizeX = 0, tSizeY = 8, tFitDropDownSizeX = true };
-		info.iconOnly = true;
-		info.isTitle = true;
-		info.notCheckable = true;
-	end
-
-	local elementDescription = TRP3_Menu.CreateMenuElementDescription();
-	elementDescription:AddInitializer(Initializer);
-	return elementDescription;
-end
-
-local function GetButtonSoundKit(description)  -- luacheck: no unused
-	return SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON;
-end
-
-local function GetCheckboxSoundKit(description)
-	if description:IsSelected() then
-		return SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON;
-	else
-		return SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF;
-	end
-end
-
 TRP3_MenuTemplates = {};
 
-function TRP3_MenuTemplates.CreateButton(text, callback, data)
-	local elementDescription = CreateButtonDescription(text);
-	elementDescription:SetData(data);
-	elementDescription:SetResponder(callback);
-	elementDescription:SetSoundKit(GetButtonSoundKit);
-	return elementDescription;
-end
-
-function TRP3_MenuTemplates.CreateCheckbox(text, isSelected, setSelected, data)
-	local elementDescription = CreateCheckboxDescription(text);
-	elementDescription:SetData(data);
-	elementDescription:SetIsSelected(isSelected);
-	elementDescription:SetResponder(setSelected);
-	elementDescription:SetResponse(TRP3_MenuResponse.Refresh);
-	elementDescription:SetSoundKit(GetCheckboxSoundKit);
-	return elementDescription;
-end
-
-function TRP3_MenuTemplates.CreateRadio(text, isSelected, setSelected, data)
-	local elementDescription = CreateRadioDescription(text);
-	elementDescription:SetData(data);
-	elementDescription:SetIsSelected(isSelected);
-	elementDescription:SetRadio(true);
-	elementDescription:SetResponder(setSelected);
-	elementDescription:SetSoundKit(GetButtonSoundKit);
-	return elementDescription;
-end
-
-function TRP3_MenuTemplates.CreateTitle(text, color)
-	local elementDescription = CreateTitleDescription(text, color);
-	return elementDescription;
-end
-
-function TRP3_MenuTemplates.CreateDivider()
-	local elementDescription = CreateDividerDescription();
-	return elementDescription;
-end
-
 function TRP3_MenuTemplates.CreateColorSelectionButton(text, color, callback, data)
-	local elementDescription = TRP3_MenuUtil.CreateButton(text, callback, data);
+	local elementDescription = MenuUtil.CreateButton(text, callback, data);
 
-	if TRP3_USE_MODERN_MENUS then
-		local function ColorSwatchInitializer(frame)
-			frame.colorSwatch = frame:AttachTemplate("TRP3_ColorSwatchTemplate");
-			frame.colorSwatch:SetPoint("LEFT");
-			frame.colorSwatch:SetSize(16, 16);
-			frame.colorSwatch:SetColor(color);
+	local function ColorSwatchInitializer(frame)
+		frame.colorSwatch = frame:AttachTemplate("TRP3_ColorSwatchTemplate");
+		frame.colorSwatch:SetPoint("LEFT");
+		frame.colorSwatch:SetSize(16, 16);
+		frame.colorSwatch:SetColor(color);
 
-			frame.fontString:SetPoint("LEFT", frame.colorSwatch, "RIGHT", 5, 0);
-		end
-
-		elementDescription:AddInitializer(ColorSwatchInitializer);
-	else
-		local function TextColorInitializer(info)
-			info.text = color:WrapTextInColorCode(info.text or "");
-		end
-
-		elementDescription:AddInitializer(TextColorInitializer);
+		frame.fontString:SetPoint("LEFT", frame.colorSwatch, "RIGHT", 5, 0);
 	end
 
+	elementDescription:AddInitializer(ColorSwatchInitializer);
 	return elementDescription;
 end
