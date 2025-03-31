524504ddb14ea0a55c58860f907bbccc1899ad05
Daniel Yates <me@meorawr.io>
2025-03-31 21:47:44 +0100

    Length constants

---
+++ totalRP3/UI/ProfileEditor/ProfileEditorConstants.lua
@@ -0,0 +1,9 @@
+-- Copyright The Total RP 3 Authors
+-- SPDX-License-Identifier: Apache-2.0
+
+TRP3_ProfileEditorLengthLimits = {
+	Currently = 140,
+	OOC = 140,
+	GlanceText = 200,
+	Notes = 2000,
+};
