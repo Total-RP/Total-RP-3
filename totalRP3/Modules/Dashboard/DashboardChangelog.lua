4ef11d696c89aae2c6ea1ed326fcef98d4674ff7
Solanya <solanya@totalrp3.info>
2025-06-17 21:25:33 +0200

    In-game changelog

--- totalRP3/Modules/Dashboard/DashboardChangelog.lua
+++ totalRP3/Modules/Dashboard/DashboardChangelog.lua
@@ -2,24 +2,16 @@
 -- SPDX-License-Identifier: Apache-2.0
 
 local CHANGELOG_TEXT = [[
-# Changelog version 3.0.10
+# Changelog version 3.0.11
 
 ## Added
 
-- Added compatibility with patch 11.1.5.
-- Added 665 icons and 96 musics from patch 11.1.0 and 11.1.5.
-  - Added 13 icons for Cataclysm Classic.
-  - Added 4 icons and 3 musics for Classic Era.
+- Added 65 icons, 7 images and 5 musics from patch 11.1.7.
 
 ## Fixed
 
-- Fixed "Open profile" not showing when right-clicking a player from a community member list.
-- Fixed an issue when using Prat where the setting to remove realm from character names wasn't working.
-
-## Removed
-
-- Removed the maximize button.
-- Removed the Wago Analytics module.
+- Fixed icon links being formatted incorrectly for MRP users.
+- Fixed issues with tutorials.
 
 For a visual guide to the major changes of version 3.0, please see [this article](https://github.com/Total-RP/Total-RP-3/wiki/Guide-to-version-3.0) on our wiki.
 ]];
