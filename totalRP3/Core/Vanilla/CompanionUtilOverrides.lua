662c44b141c62bdf10a4aeafbf906932bc42a09b
Daniel Yates <me@meorawr.io>
2025-07-26 23:55:10 +0100

    Misc. bug fixes and cleanup
    Some parts were using a '.id' field when reading/writing to the static
    pet data tables instead of a '.petID' field.
    
    Reorganize a few functions and combine the two frames into one for
    managing data load callbacks and active pet monitoring.
    
    Additionally, unload the item/spell load events when we no longer
    need them.

--- totalRP3/Core/Vanilla/CompanionUtilOverrides.lua
+++ totalRP3/Core/Vanilla/CompanionUtilOverrides.lua
@@ -17,6 +17,27 @@ local staticCompanionPetsByCreatureID = {};
 local staticCompanionPetsBySpellID = {};
 local staticMountsByID = {};
 
+local function RegisterCompanionPet(petInfo)
+	staticCompanionPetsByID[petInfo.petID] = petInfo;
+	staticCompanionPetsByCreatureID[petInfo.creatureID] = petInfo;
+	staticCompanionPetsBySpellID[petInfo.spellID] = petInfo;
+end
+
+local function RegisterCompanionMount(mountInfo)
+	staticMountsByID[mountInfo.mountID] = mountInfo;
+end
+
+local function SetSummonedCompanionPetID(petID)
+	-- The summoned pet ID is stored in saved variables to allow it to persist
+	-- across UI reloads.
+
+	if not TRP3_Companions then
+		TRP3_Companions = {};
+	end
+
+	TRP3_Companions.summonedPetID = petID;
+end
+
 function TRP3_CompanionUtil.CanRenameCompanionPets()
 	return false;
 end
@@ -69,7 +90,6 @@ function TRP3_CompanionUtil.GetCompanionPetUnitName(unitToken)
 		return staticPetInfo.name;
 	end
 end
-
 
 function TRP3_CompanionUtil.GetSummonedCompanionPetID()
 	if TRP3_Companions then
@@ -153,77 +173,127 @@ function TRP3_CompanionUtil.GetSummonedMountID()
 	return cachedSummonedMountID;
 end
 
--- On clients that don't have a pet or mount journal, we statically embed
--- a database with just enough data to allow us to build our own fake version
--- of the journals in-game.
---
--- This static data doesn't include localized strings, and needs to be
--- filtered based on what's actually available in the client. To do this we
--- need to do some sparse loads for item and spell data prior to registering
--- the companion.
+local CompanionStateDriverMixin = {};
 
-local CompanionDataLoaderMixin = {};
-
-function CompanionDataLoaderMixin:OnLoad()
+function CompanionStateDriverMixin:OnLoad()
 	self.pendingSpells = {};
 	self.pendingItems = {};
 
-	self:RegisterEvent("ITEM_DATA_LOAD_RESULT");
-	self:RegisterEvent("SPELL_DATA_LOAD_RESULT");
+	self:RegisterEvent("PLAYER_ENTERING_WORLD");
+	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "player");
 end
 
-function CompanionDataLoaderMixin:AddLoadCallback(callbackTable, recordID, callbackFunction)
-	local callbacks = callbackTable[recordID];
+function CompanionStateDriverMixin:OnEvent(event, ...)
+	if event == "UNIT_SPELLCAST_SUCCEEDED" then
+		self:OnUnitSpellcastSucceeded(...);
+	elseif event == "ITEM_DATA_LOAD_RESULT" then
+		self:OnItemDataLoadResult(...);
+	elseif event == "SPELL_DATA_LOAD_RESULT" then
+		self:OnSpellDataLoadResult(...);
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		self:OnPlayerEnteringWorld(...);
+	end
+end
+
+function CompanionStateDriverMixin:OnPlayerEnteringWorld(isInitialLogin)
+	-- Pets don't remain summoned across logins, so clear any old state.
+	if isInitialLogin then
+		SetSummonedCompanionPetID(nil);
+	end
+end
+
+function CompanionStateDriverMixin:OnItemDataLoadResult(itemID, success)
+	self:InvokeDataLoadCallbacks(self.pendingItems, itemID, success);
+end
+
+function CompanionStateDriverMixin:OnSpellDataLoadResult(spellID, success)
+	self:InvokeDataLoadCallbacks(self.pendingSpells, spellID, success);
+end
+
+function CompanionStateDriverMixin:OnUnitSpellcastSucceeded(_, castGUID)
+	-- On client flavors without a pet journal we need to be creative with how we
+	-- determine what companion pet is actually summoned.
+	--
+	-- Our approach is to monitor for successful spellcasts whose spell IDs
+	-- are associated with that of a known companion pet. We assume that the
+	-- last successful cast will represent the current battle pet.
+	--
+	-- Note that we can't tell when a companion pet is dismissed, so our query
+	-- data will always contain the data for the last-successful cast even if
+	-- that cast dismissed the pet. Realistically this should be fine since
+	-- if it's dismissed other players can't see the unit to request the data
+	-- anyway.
+	--
+	-- For persistence across UI reloads we store the summoned pet data in our
+	-- saved variables, and reset it upon initial login of a new character.
+
+	local spellID = tonumber((select(6, string.split("-", castGUID, 7))));
+	local staticPetInfo = staticCompanionPetsBySpellID[spellID];
+
+	if staticPetInfo then
+		SetSummonedCompanionPetID(staticPetInfo.petID);
+	end
+end
+
+function CompanionStateDriverMixin:AddDataLoadCallback(registry, recordID, callbackFunction)
+	local callbacks = registry[recordID];
 
 	if not callbacks then
 		callbacks = {};
-		callbackTable[recordID] = callbacks;
+		registry[recordID] = callbacks;
 	end
 
 	table.insert(callbacks, callbackFunction);
 end
 
-function CompanionDataLoaderMixin:InvokeLoadCallbacks(callbackTable, recordID, ...)
-	local callbacks = callbackTable[recordID];
-	callbackTable[recordID] = nil;
+function CompanionStateDriverMixin:InvokeDataLoadCallbacks(registry, recordID, ...)
+	local callbacks = registry[recordID];
+	registry[recordID] = nil;
 
-	for _, callbackFunction in ipairs(callbacks) do
-		securecallfunction(callbackFunction, recordID, ...);
+	if callbacks then
+		for _, callbackFunction in ipairs(callbacks) do
+			securecallfunction(callbackFunction, recordID, ...);
+		end
 	end
 end
 
-function CompanionDataLoaderMixin:RequestLoadItemData(itemID, callbackFunction)
-	self:AddLoadCallback(self.pendingItems, itemID, callbackFunction);
+function CompanionStateDriverMixin:HasPendingDataLoadCallbacks(registry)
+	return (next(registry) ~= nil);
+end
+
+function CompanionStateDriverMixin:RequestLoadItemData(itemID, callbackFunction)
+	self:AddDataLoadCallback(self.pendingItems, itemID, callbackFunction);
+	self:UpdateEventRegistrations();
+
+	-- This call can synchronously fire the event, so we need to sequence it
+	-- after updating event registrations.
 	C_Item.RequestLoadItemDataByID(itemID);
 end
 
-function CompanionDataLoaderMixin:RequestLoadSpellData(spellID, callbackFunction)
-	self:AddLoadCallback(self.pendingSpells, spellID, callbackFunction);
+function CompanionStateDriverMixin:RequestLoadSpellData(spellID, callbackFunction)
+	self:AddDataLoadCallback(self.pendingSpells, spellID, callbackFunction);
+	self:UpdateEventRegistrations();
+
+	-- This call can synchronously fire the event, so we need to sequence it
+	-- after updating event registrations.
 	C_Spell.RequestLoadSpellData(spellID);
 end
 
-function CompanionDataLoaderMixin:OnEvent(event, ...)
-	if event == "ITEM_DATA_LOAD_RESULT" then
-		local itemID, success = ...;
-		self:InvokeLoadCallbacks(self.pendingItems, itemID, success);
-	elseif event == "SPELL_DATA_LOAD_RESULT" then
-		local spellID, success = ...;
-		self:InvokeLoadCallbacks(self.pendingSpells, spellID, success);
+function CompanionStateDriverMixin:SetEventRegistered(eventName, registered)
+	if registered then
+		self:RegisterEvent(eventName);
+	else
+		self:UnregisterEvent(eventName);
 	end
 end
 
-local CompanionDataLoader = CreateFrame("Frame")
-FrameUtil.SpecializeFrameWithMixins(CompanionDataLoader, CompanionDataLoaderMixin);
-
-local function RegisterCompanionPet(petInfo)
-	staticCompanionPetsByID[petInfo.id] = petInfo;
-	staticCompanionPetsByCreatureID[petInfo.creatureID] = petInfo;
-	staticCompanionPetsBySpellID[petInfo.spellID] = petInfo;
+function CompanionStateDriverMixin:UpdateEventRegistrations()
+	self:SetEventRegistered("ITEM_DATA_LOAD_RESULT", self:HasPendingDataLoadCallbacks(self.pendingItems));
+	self:SetEventRegistered("SPELL_DATA_LOAD_RESULT", self:HasPendingDataLoadCallbacks(self.pendingSpells));
 end
 
-local function RegisterCompanionMount(mountInfo)
-	staticMountsByID[mountInfo.mountID] = mountInfo;
-end
+local CompanionStateDriver = CreateFrame("Frame");
+FrameUtil.SpecializeFrameWithMixins(CompanionStateDriver, CompanionStateDriverMixin);
 
 do
 	for _, petInfo in ipairs(TRP3_CompanionPetData) do
@@ -233,14 +303,14 @@ do
 			end
 
 			petInfo = CreateFromMixins(petInfo);
-			petInfo.id = tostring(petInfo.speciesID);
+			petInfo.petID = tostring(petInfo.speciesID);
 			petInfo.name = C_Item.GetItemNameByID(itemID);
 			petInfo.icon = C_Item.GetItemIconByID(itemID);
 
 			RegisterCompanionPet(petInfo);
 		end
 
-		CompanionDataLoader:RequestLoadItemData(petInfo.itemID, OnPetItemLoaded);
+		CompanionStateDriver:RequestLoadItemData(petInfo.itemID, OnPetItemLoaded);
 	end
 
 	local playerFaction = UnitFactionGroup("player");
@@ -260,66 +330,7 @@ do
 
 		-- Only load mounts relevant for the player faction.
 		if mountInfo.factionGroup == nil or mountInfo.factionGroup == playerFaction then
-			CompanionDataLoader:RequestLoadSpellData(mountInfo.spellID, OnMountSpellLoaded);
+			CompanionStateDriver:RequestLoadSpellData(mountInfo.spellID, OnMountSpellLoaded);
 		end
 	end
 end
-
--- On client flavors without a pet journal we need to be creative with how we
--- determine what companion pet is actually summoned.
---
--- Our approach is to monitor for successful spellcasts whose spell IDs
--- are associated with that of a known companion pet. We assume that the
--- last successful cast will represent the current battle pet.
---
--- Note that we can't tell when a companion pet is dismissed, so our query
--- data will always contain the data for the last-successful cast even if
--- that cast dismissed the pet. Realistically this should be fine since
--- if it's dismissed other players can't see the unit to request the data
--- anyway.
---
--- For persistence across UI reloads we store the summoned pet data in our
--- saved variables, and reset it upon initial login of a new character.
-
-local CompanionPetSummonListenerMixin = {};
-
-function CompanionPetSummonListenerMixin:OnLoad()
-	self:RegisterUnitEvent("UNIT_SPELLCAST_SUCCEEDED", "player");
-	self:RegisterEvent("PLAYER_ENTERING_WORLD");
-end
-
-function CompanionPetSummonListenerMixin:OnEvent(event, ...)
-	if event == "UNIT_SPELLCAST_SUCCEEDED" then
-		self:OnUnitSpellcastSucceeded();
-	elseif event == "PLAYER_ENTERING_WORLD" then
-		self:OnPlayerEnteringWorld(...);
-	end
-end
-
-function CompanionPetSummonListenerMixin:OnUnitSpellcastSucceeded(_, castGUID)
-	local spellID = tonumber((select(6, string.split("-", castGUID, 7))));
-	local staticPetInfo = staticCompanionPetsBySpellID[spellID];
-
-	if staticPetInfo then
-		self:SetSummonedPetID(staticPetInfo.id);
-	end
-end
-
-function CompanionPetSummonListenerMixin:OnPlayerEnteringWorld(isInitialLogin)
-	-- Pets don't remain summoned across logins, so clear any old state.
-
-	if isInitialLogin then
-		self:SetSummonedPetID(nil);
-	end
-end
-
-function CompanionPetSummonListenerMixin:SetSummonedPetID(petID)
-	-- The summoned pet ID is stored in saved variables to allow it to persist
-	-- across UI reloads.
-
-	if TRP3_Companions then
-		TRP3_Companions.summonedPetID = petID;
-	end
-end
-
-FrameUtil.SpecializeFrameWithMixins(CreateFrame("Frame"), CompanionPetSummonListenerMixin);
