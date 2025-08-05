d8b17ed5dba30d4d69c0c85e2686c37761e79336
Raenore <172234435+Raenore@users.noreply.github.com>
2025-08-05 16:36:40 +0200

    Simple maths

--- totalRP3/Modules/Flyway/Flyway.lua
+++ totalRP3/Modules/Flyway/Flyway.lua
@@ -10,18 +10,18 @@ if not TRP3_Flyway then
 end
 
 local function applyPatches(fromBuild, toBuild)
-	for i = fromBuild + 1, toBuild do
+	for i = fromBuild, toBuild do
 		if type(TRP3_API.flyway.patches[tostring(i)]) == "function" then
 			TRP3_API.Logf("Applying patch %s", i);
 			TRP3_API.flyway.patches[tostring(i)]();
 		end
 	end
-	TRP3_Flyway.log = ("Patch applied from %s to %s on %s"):format(fromBuild, toBuild, date("%d/%m/%y %H:%M:%S"));
+	TRP3_Flyway.log = ("Patch applied from %s to %s on %s"):format(fromBuild - 1, toBuild, date("%d/%m/%y %H:%M:%S"));
 end
 
 function TRP3_API.flyway.applyPatches()
 	if not TRP3_Flyway.currentBuild or TRP3_Flyway.currentBuild < SCHEMA_VERSION then
-		applyPatches((TRP3_Flyway.currentBuild or 0), SCHEMA_VERSION);
+		applyPatches((TRP3_Flyway.currentBuild or 0) + 1, SCHEMA_VERSION);
 	end
 	TRP3_Flyway.currentBuild = SCHEMA_VERSION;
 end
