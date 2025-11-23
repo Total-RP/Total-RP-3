5cbcfd0a5af985bc38267906b2b2712c514548a0
Daniel Yates <me@meorawr.io>
2025-11-23 15:47:20 +0000

    Prefer using IsAddOnEnabledForCurrentCharacter

--- totalRP3/Modules/ModuleManagement.lua
+++ totalRP3/Modules/ModuleManagement.lua
@@ -110,7 +110,7 @@ local function checkModuleDependency(_, dependency)
 		return MODULE_REGISTRATION[dependency_id] and MODULE_REGISTRATION[dependency_id].version >= dependency_version and
 			MODULE_ACTIVATION[dependency_id] ~= false;
 	else
-		return TRP3_API.utils.IsAddOnEnabled(dependency_id);
+		return AddOnUtil.IsAddOnEnabledForCurrentCharacter(dependency_id);
 	end
 end
 
