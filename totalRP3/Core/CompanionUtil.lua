31a72b81fde3b163d31fc86ff32b39f4a831162b
Daniel Yates <me@meorawr.net>
2026-09-18 20:22:51 +0100

    Restructure CompanionUtil implementation (#1363)
    * Relocate journal data provider to separate file
    
    * Convert classic overrides to a static data provider

--- totalRP3/Core/CompanionUtil.lua
+++ totalRP3/Core/CompanionUtil.lua
@@ -1,47 +1,34 @@
 -- Copyright The Total RP 3 Authors
 -- SPDX-License-Identifier: Apache-2.0
 
-local cachedSummonedMountID = nil;
-
 TRP3_CompanionUtil = {};
 
+function TRP3_CompanionUtil.GetDataProvider()
+	return nil;
+end
+
 function TRP3_CompanionUtil.CanRenameCompanionPets()
-	return true;
+	return TRP3_CompanionUtil.GetDataProvider():CanRenameCompanionPets();
 end
 
 function TRP3_CompanionUtil.GetCompanionPetIDs()
-	return C_PetJournal.GetOwnedPetIDs();
+	return TRP3_CompanionUtil.GetDataProvider():GetCompanionPetIDs();
 end
 
 function TRP3_CompanionUtil.GetCompanionPetInfo(petID)
-	local speciesID, customName, _, _, _, _, _, speciesName, icon, _, _, _, description = C_PetJournal.GetPetInfoByPetID(petID);
-
-	if not speciesID then
-		return;
-	end
-
-	return {
-		id = petID,
-		name = (customName ~= "") and customName or speciesName,
-		customName = customName ~= "" and customName or nil,
-		icon = icon,
-		description = description,
-		speciesName = speciesName,
-		isCollected = true,
-	};
+	return TRP3_CompanionUtil.GetDataProvider():GetCompanionPetInfo(petID);
 end
 
 function TRP3_CompanionUtil.IsCompanionPetUnit(unitToken)
-	return UnitIsBattlePetCompanion(unitToken);
+	return TRP3_CompanionUtil.GetDataProvider():IsCompanionPetUnit(unitToken);
 end
 
 function TRP3_CompanionUtil.GetCompanionPetUnitName(unitToken)
-	local name = UnitNameUnmodified(unitToken);
-	return name;
+	return TRP3_CompanionUtil.GetDataProvider():GetCompanionPetUnitName(unitToken);
 end
 
 function TRP3_CompanionUtil.GetSummonedCompanionPetID()
-	return C_PetJournal.GetSummonedPetGUID();
+	return TRP3_CompanionUtil.GetDataProvider():GetSummonedCompanionPetID();
 end
 
 function TRP3_CompanionUtil.EnumerateCompanionPets()
@@ -61,59 +48,19 @@ function TRP3_CompanionUtil.EnumerateCompanionPets()
 end
 
 function TRP3_CompanionUtil.GetMountIDs()
-	return C_MountJournal.GetMountIDs();
+	return TRP3_CompanionUtil.GetDataProvider():GetMountIDs();
 end
 
 function TRP3_CompanionUtil.GetMountInfo(mountID)
-	local name, spellID, icon, _, _, _, _, _, _, _, isCollected = C_MountJournal.GetMountInfoByID(mountID);
-	local _, description = C_MountJournal.GetMountInfoExtraByID(mountID);
-
-	if not spellID then
-		return;
-	end
-
-	return {
-		id = mountID,
-		name = name,
-		icon = icon,
-		description = description,
-		isCollected = isCollected,
-		spellID = spellID,
-	};
+	return TRP3_CompanionUtil.GetDataProvider():GetMountInfo(mountID);
 end
 
 function TRP3_CompanionUtil.GetMountSpellID(mountID)
-	local _, spellID = C_MountJournal.GetMountInfoByID(mountID);
-	return spellID;
+	return TRP3_CompanionUtil.GetDataProvider():GetMountSpellID(mountID);
 end
 
 function TRP3_CompanionUtil.GetSummonedMountID()
-	if not IsMounted() then
-		return nil;
-	end
-
-	-- Is the last mount that we tested still the current summon?
-	if cachedSummonedMountID ~= nil then
-		local _, _, _, isActive = C_MountJournal.GetMountInfoByID(cachedSummonedMountID);
-
-		if not isActive then
-			cachedSummonedMountID = nil;
-		end
-	end
-
-	-- Has our cache been invalidated? If so, scan the journal.
-	if cachedSummonedMountID == nil then
-		for _, mountID in ipairs(TRP3_CompanionUtil.GetMountIDs()) do
-			local _, _, _, isActive = C_MountJournal.GetMountInfoByID(mountID);
-
-			if isActive then
-				cachedSummonedMountID = mountID;
-				break;
-			end
-		end
-	end
-
-	return cachedSummonedMountID;
+	return TRP3_CompanionUtil.GetDataProvider():GetSummonedMountID();
 end
 
 function TRP3_CompanionUtil.EnumerateMounts()
